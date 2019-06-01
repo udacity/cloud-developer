4a6ef6f87a4dc8fc12e8f1f1f54e02b35504c8f0
LalitKishor <lalitsinghbanna05@gmail.com>
2019-06-01 14:29:22 +0530

    getFilterImage API

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -17,9 +17,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
+  //    2. call filterImageFromURL(image_url) to filter the image 
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
@@ -30,17 +29,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/filteredimage", async (req, res) => {
+    try {
+      const imgPath = await filterImageFromURL(req.query.image_url);
+      const readFilePromise = require('util').promisify(require('fs').readFile);
+      const greyImage = await readFilePromise(imgPath);
+      res.header('content-type', 'image/jpeg').send(greyImage);
+      deleteLocalFiles([imgPath]);
+    } catch (err) {
+      console.log(err);
+      if (err.code && err.code == 'ERR_BAD_URL')
+        res.status(422).send({
+          error: true,
+          message: 'Cannot Process Specified URL'
+        })
+      else
+        res.status(500).send({
+          error: true,
+          message: 'Internal Server Error'
+        })
+    }
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
