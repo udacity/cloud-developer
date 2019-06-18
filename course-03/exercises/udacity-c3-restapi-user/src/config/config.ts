fc85a4a43747bf3dbe83aa020873107b2132bca2
Sebastian Scheele <sebastian@loodse.com>
2019-06-17 23:31:48 -0700

    Update services

--- course-03/exercises/udacity-c3-restapi-user/src/config/config.ts
+++ course-03/exercises/udacity-c3-restapi-user/src/config/config.ts
@@ -7,7 +7,8 @@ export const config = {
     "dialect": "postgres",
     "aws_reigion": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET
+    "aws_media_bucket": process.env.AWS_BUCKET,
+    "url": process.env.URL
   },
   "prod": {
     "username": "",
--- course-03/exercises/udacity-c3-restapi-user/src/server.ts
+++ course-03/exercises/udacity-c3-restapi-user/src/server.ts
@@ -4,8 +4,10 @@ import { sequelize } from './sequelize';
 import { IndexRouter } from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
-
+import { config } from './config/config';
 import { V0MODELS } from './controllers/v0/model.index';
+
+const c = config.dev;
 
 (async () => {
   await sequelize.addModels(V0MODELS);
@@ -18,7 +20,7 @@ import { V0MODELS } from './controllers/v0/model.index';
 
   //CORS Should be restricted
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Origin", c.url);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
@@ -33,7 +35,7 @@ import { V0MODELS } from './controllers/v0/model.index';
 
   // Start the Server
   app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
+      console.log( `server running ` + c.url );
       console.log( `press CTRL+C to stop server` );
   } );
 })();
