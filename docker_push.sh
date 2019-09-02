f6cf30083a847f973caedc16f110d3f0b18a512f
Wei Lip <weilip@fairtide.co>
2019-09-02 19:25:21 +0800

    Update docker push

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push weilip/udacity-restapi-user
 docker push weilip/udacity-restapi-feed
