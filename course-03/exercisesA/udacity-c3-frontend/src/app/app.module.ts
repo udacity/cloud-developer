bc5c4e853383f3559a9be1ea296e8113d08961b3
Nada Albar <nadaalbar@yahoo.com>
2019-09-27 16:19:04 +0300

    adding exerciseA folder

---
+++ course-03/exercisesA/udacity-c3-frontend/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MenubarComponent } from './menubar/menubar.component';
+
+import { AuthModule } from './auth/auth.module';
+import { ApiService } from './api/api.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenubarComponent
+  ],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AuthModule
+  ],
+  providers: [
+    ApiService,
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
