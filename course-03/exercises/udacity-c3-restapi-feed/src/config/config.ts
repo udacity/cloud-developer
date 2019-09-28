bf71c417d7e702477748827629f59182fd403677
JHPolarBear <jhpark8904@gmail.com>
2019-09-28 18:07:07 +0900

    change config env value

--- course-03/exercises/udacity-c3-restapi-feed/src/config/config.ts
+++ course-03/exercises/udacity-c3-restapi-feed/src/config/config.ts
@@ -2,13 +2,14 @@ export const config = {
   "dev": {
     "username": process.env.POSTGRESS_USERNAME,
     "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
+    "database": process.env.POSTGRESS_DATABASE,
     "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
     "aws_reigion": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET,
-    "url": process.env.URL    
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
+    "url": process.env.URL,  
+    "JWT_SECRET": process.env.JWT_SECRET
   },
   "prod": {
     "username": "",
