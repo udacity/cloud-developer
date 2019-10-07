f7ffa15c207300a280097812d499149f3de7fe3f
Abdulmohsen Alkhamis <Abdulmohsen-alkhamis@hotmail.com>
2019-10-07 23:43:26 +0300

    Setting New Endpoing
    Setting an endpoint for getting an image and pass it to a filter function and get a filtered image as result

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -30,7 +30,20 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+  app.get( "/filteredimage", async ( req , res ) => {
+    let {image_url} = req.query;
+    let result = await filterImageFromURL(image_url);
+    if (image_url) {      
+      res.status(200).sendFile(result, () => {deleteLocalFiles([result])});
+    } else {
+      return res.status(404).send("Invalid Request!")
+    }
+  } );
   
+
+
+
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
