460ee38c4ae31b184a64d2d27b12812a2bceab30
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2019-10-19 23:53:19 +1300

    created endpoint

--- course-02/project/image-filter-starter-code/README.md
+++ course-02/project/image-filter-starter-code/README.md
@@ -1,6 +1,6 @@
 # Udagram Image Filtering Microservice
 
-# Edited by Sandeep Perera
+## Edited by Sandeep Perera
 
 Udagram is a simple cloud application developed alongside the Udacity Cloud Engineering Nanodegree. It allows users to register and log into a web client, post photos to the feed, and process photos using an image filtering microservice.
 
--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -17,7 +18,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -28,6 +28,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get( "/filteredimage/", async ( req, res ) => {
+    let { image_url } = req.query;
+
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`image_url is required`);
+    }
+
+    try {
+      const filteredpath = await filterImageFromURL(image_url);
+      await res.status(200).sendFile(filteredpath, {}, (err) => {
+        if (err) { return res.status(422).send(`unable to process image`); }
+        // Deleting the used image file.
+        deleteLocalFiles([filteredpath])
+      });
+    }
+    catch (err){
+      res.status(422).send(`unable to process image`);
+    }
+  } );
 
   //! END @TODO1
   
