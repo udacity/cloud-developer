086c7be0d6cc3da3051f8563b10f5ee71743ed2a
Daniel Wu <daniel.wu@sonder.com>
2019-10-26 19:23:43 -0700

    attempt at fetching cert

--- c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -12,7 +12,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-x1n-gpar.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -57,11 +57,19 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  if (jwt.header.alg !== 'RS256') {
+    console.log('NOT RS256', jwt)
+  }
+  const key = await getJwks(jwt.header)
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  return verify(
+    token, // Token from an HTTP header to validate
+    key.publicKey, // A certificate copied from Auth0 website
+    { algorithms: ['RS256'] } // We need to specify that we use the RS256 algorithm
+  ) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +82,63 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+interface SigningKey {
+  kid: string
+  nbf: string
+  publicKey: string
+}
+
+async function getJwks(header: Jwt['header']) {
+  const response = await Axios.get(jwksUrl, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  const { keys } = response.data
+  console.log('keys :', keys)
+  const signingKeys = getSigningKeys(keys)
+  console.log('signingKeys :', signingKeys)
+  const signingKey = getSigningKey(header.kid, signingKeys)
+  console.log('signingKey :', signingKey)
+  return signingKey
+}
+
+function getSigningKeys(keys: any[]): SigningKey[] {
+  if (!keys || !keys.length) {
+    throw new Error('The JWKS endpoint did not contain any keys')
+  }
+
+  const signingKeys = keys
+    .filter(
+      key =>
+        key.use === 'sig' && // JWK property `use` determines the JWK is for signing
+        key.kty === 'RSA' && // We are only supporting RSA (RS256)
+        key.kid && // The `kid` must be present to be useful for later
+        ((key.x5c && key.x5c.length) || (key.n && key.e)) // Has useful public keys
+    )
+    .map(key => {
+      return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) }
+    })
+
+  if (!signingKeys.length) {
+    throw new Error('The JWKS endpoint did not contain any signing keys')
+  }
+
+  return signingKeys
+}
+
+function certToPEM(cert: string) {
+  cert = cert.match(/.{1,64}/g).join('\n')
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`
+  return cert
+}
+
+function getSigningKey(kid: string, keys: SigningKey[]): SigningKey {
+  const signingKey = keys.find(key => key.kid === kid)
+  if (!signingKey) {
+    throw new Error(`Unable to find a signing key that matches '${kid}'`)
+  }
+  return signingKey
 }
--- c4-final-project-starter-code/client/src/config.ts
+++ c4-final-project-starter-code/client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-x1n-gpar.auth0.com', // Auth0 domain
+  clientId: 'LRq020yVnlZdqBwSppm3X7VXgLFd8Ar1', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
