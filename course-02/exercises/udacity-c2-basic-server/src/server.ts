c12eeff344c2be8cd61d65d8a8c2b7a4cdd3799f
Kranthi <kranthi.kiran@ge.com>
2019-12-03 10:35:24 -0800

    added todo APIs

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -2,6 +2,7 @@ import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 import { Car, cars as cars_list } from './cars';
+import { parse } from 'path';
 
 (async () => {
   let cars:Car[]  = cars_list;
@@ -70,13 +71,81 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+app.get("/cars", (req: Request, res: Response) => {
+  let { make } = req.query;
+
+  let cars_list = cars;
+
+  //filter only if make value is provided
+  if(make){
+    cars_list = cars.filter(car => car.make === make);
+  }
+
+  //check if cars_list is empty
+  if(cars_list && cars_list.length === 0){
+    return res.status(200)
+            .send(`no cars found for ${make}`);
+  }
+
+  return res.status(200)
+              .send(cars_list);
+});
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get("/cars/:id", (req: Request, res: Response) => {
+    //destruct to fetch id from paramsters
+    let { id } = req.params;
+
+    let cars_list = cars;
+
+    //check if an id has been provided
+    if(id){
+      cars_list = cars.filter(car => car.id === parseInt(id));
+    }
+
+    console.log(cars_list);
+
+    return res.status(200)
+              .send(cars_list);
+  });
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+app.post("/cars", 
+  async(req: Request, res: Response) => {
+
+  // destruct request body to a car object variable 
+  let { id, cost, model, type, make} = req.body;
+
+  const new_car : Car = {
+    id: id, cost: cost, type: type, make: make, model: model
+  };
+
+  // verify if all inputs are provided
+  if(!id || !cost || !model || !type || !make){
+    return res.status(400)
+            .send("all inputs of Car object are not provided");
+  }
+
+  // check if an object with the same id exists
+  let exist_car = cars;
+  
+  exist_car = cars.filter(car => car.id === parseInt(id));
+  
+  if(exist_car.length > 0){
+    return res.status(400)
+            .send("Car object with this id already exists");
+  }
+
+  // add the new car
+  cars.push(new_car);
+
+  return res.status(200)
+          .send(cars);
+
+  });
 
   // Start the Server
   app.listen( port, () => {
