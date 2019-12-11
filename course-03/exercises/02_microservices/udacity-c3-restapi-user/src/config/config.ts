2f5bf3d08a0873e6dcd9db3c5aa832f69b43eb68
Aleksandar <aleks.mkd@gmail.com>
2019-12-11 15:19:59 -0800

    Changes in names of 2 config entries for the Env vars - AWS_MEDIA_BUCKET and POSTGRESS_DATABASE.

--- course-03/exercises/02_microservices/udacity-c3-restapi-user/src/config/config.ts
+++ course-03/exercises/02_microservices/udacity-c3-restapi-user/src/config/config.ts
@@ -2,12 +2,12 @@ export const config = {
   "dev": {
     "username": process.env.POSTGRESS_USERNAME,
     "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
+    "database": process.env.POSTGRESS_DATABASE,
     "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
     "aws_reigion": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
