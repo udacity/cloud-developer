9e500afd94f5242e3b0746d36738678dcfd058b8
Aleksandar <aleks.mkd@gmail.com>
2019-12-11 15:17:11 -0800

    Initial solution.

---
+++ course-03/exercises/02_microservices/udacity-c3-restapi-user/src/controllers/v0/index.router.ts
@@ -0,0 +1,12 @@
+import { Router, Request, Response } from 'express';
+import { UserRouter } from './users/routes/user.router';
+
+const router: Router = Router();
+
+router.use('/users', UserRouter);
+
+router.get('/', async (req: Request, res: Response) => {    
+    res.send(`V0`);
+});
+
+export const IndexRouter: Router = router;
