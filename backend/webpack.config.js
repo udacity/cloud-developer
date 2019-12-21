2fd2b6f1d7ad456b68a185257c35b484d2469174
Dan Wu <daniel.wu@sonder.com>
2019-12-20 23:39:37 -0800

    webpack revert

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   externals: [
     {
-      // formidable: 'commonjs formidable',
+      formidable: 'commonjs formidable',
     },
   ],
   // externals: [nodeExternals()],
