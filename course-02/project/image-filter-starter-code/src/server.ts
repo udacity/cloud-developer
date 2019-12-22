e84449e173920b8c07456bb32c94e0f1c4cac0da
aisha.elbadrawy <aisha.elbadrawy@future-group.com>
2019-12-22 11:32:41 +0200

    completed todo in server.ts

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import urlExists from 'url-exists';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -30,6 +31,65 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+
+  //testing url
+  //http://localhost:8082/filteredimage?image_url=https://homepages.cae.wisc.edu/~ece533/images/airplane.png
+  //http://localhost:{{PORT}}/filteredimage?image_url=https://timedotcom.files.wordpress.com/2019/03/kitten-report.jpg
+
+  app.get("/filteredimage", async ( req, res) => {
+    const image_url = req.query.image_url;
+
+    const promise = new Promise((resolve, reject) => {
+        urlExists(image_url, function(err, exists) {
+        console.log(exists)
+        resolve(exists)
+    
+       }); })
+
+      promise.catch(err => error_handler(err,res)).then(is_vlaid => filter_handler(image_url,is_vlaid,res))
+           
+           
+   
+  } );
+
+
+async function error_handler(err,res){
+   console.log("error"+err)
+  return res.status(400).send({
+               message: "The image url does not exist or is malformed"
+                });
+}
+
+
+//few questions, will it matter if i use return with res.status
+// and what's the diffrence betwen  res.status and res.sendStaus
+//when I used the latter i faced and error UnhandledPromiseRejectionWarning: Error: Can't set headers after they are sent.
+async function filter_handler(image_url, is_valid, res){
+
+
+             if(!is_valid) 
+               return res.status(400).send({
+               message: "The image url does not exist or is malformed"
+                });
+           else{
+              try{
+    
+                const filteredImage = await filterImageFromURL(image_url);
+                res.sendFile(filteredImage, () =>
+                deleteLocalFiles([filteredImage])
+                   );
+
+                  }
+
+              catch (error){
+                 return res.status(400).send({
+                  message: "something went wrong while processing image url, please make sure the url points to an image"
+                  });
+               }
+               }
+
+}
+ 
   
   // Root Endpoint
   // Displays a simple message to the user
