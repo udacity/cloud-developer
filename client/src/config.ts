7a90ebb23923e50f1c4cba3a7766cb3d90c143b3
Daniel Wu <im.danwu@gmail.com>
2019-12-27 21:43:59 -0800

    try this

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,11 @@
 const apiId = 'yq6nbb4r5k'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 // export const apiEndpoint = `http://localhost:4000`
+export const callbackEndpoint = 'https://fervent-mirzakhani-5d72eb.netlify.com'
+// export const callbackEndpoint =  http://localhost:3000
 
 export const authConfig = {
   domain: 'dev-x1n-gpar.auth0.com', // Auth0 domain
   clientId: 'LRq020yVnlZdqBwSppm3X7VXgLFd8Ar1', // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: `${callbackEndpoint}/callback`
 }
