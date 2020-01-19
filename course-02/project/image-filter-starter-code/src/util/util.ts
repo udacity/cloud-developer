2f4f1118ab0b28b65ce55e60993b0737a3fb0d57
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-01-19 00:30:08 -0300

    refactor: Change code to return error

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -9,16 +9,18 @@ import Jimp = require('jimp');
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+    return new Promise( async (resolve, reject) => {
+        const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+        await Jimp.read(inputURL)
+            .then(photo => {
+                return photo
+                    .resize(256, 256) // resize
+                    .quality(60) // set JPEG quality
+                    .greyscale() // set greyscale
+                    .write(__dirname + outpath, (img) => {
+                        resolve(__dirname + outpath);
+                    });
+            }).catch(err => reject(err));
     });
 }
 
