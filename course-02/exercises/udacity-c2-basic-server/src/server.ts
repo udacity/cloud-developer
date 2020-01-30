325cc529ab1833542accef06c60474e2622e3a25
madalinamihaelamladin <madalina.mihaela.mladin@gmail.com>
2020-01-30 18:54:52 +0200

    basic server development

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -70,6 +70,20 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get("/cars/", ( req: Request, res: Response ) => {
+      //destruct our query parameters
+      let { make } = req.query;
+
+      let cars_list = cars;
+
+      // if we have am optional query parameter, filter by it
+      if (make) {
+        cars_list = cars.filter((car) => car.make === make);
+      }
+
+      // return the resulting list along with 200 success
+      res.status(200).send(cars_list);
+  });
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
