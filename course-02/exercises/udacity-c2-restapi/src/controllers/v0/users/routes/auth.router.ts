61779e644a43b3fb7140ae1d99e0c0b859d6c0a8
Rachel Tumulak <rmktumulak@gmail.com>
2020-02-26 04:31:28 +0800

    Add brcrypt functions

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -12,10 +12,18 @@ const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
     //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(plainTextPassword, salt);
+
+    // const compare = await bcrypt.compare(plainTextPassword, hash);
+    return hash;
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
+    const compare = await bcrypt.compare(plainTextPassword, hash);
+    return compare;
 }
 
 function generateJWT(user: User): string {
