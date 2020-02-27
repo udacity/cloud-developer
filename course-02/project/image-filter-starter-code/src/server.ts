2e01e8bde10951f95be740eeded82c34f4676f40
Rachel Tumulak <rmktumulak@gmail.com>
2020-02-27 18:16:08 +0800

    Ready for deployment

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Router, Request, Response } from 'express';
+import express, { Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -22,7 +22,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
-  // QUERY PAR
+  // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
@@ -30,6 +30,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+  
   app.get("/filteredimage",
     async (req: Request, res: Response) => {
       const { image_url } = req.query;
