268f016aea2ca48b45f83f9bf15c76389a5c70fa
Everistus Olumese <everistusemail@gmail.com>
2020-03-01 14:49:41 +0100

    Added image filtering endpoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import bodyParser from "body-parser";
 import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 const validUrl = require("valid-url");
@@ -8,42 +8,46 @@ const validUrl = require("valid-url");
   const app = express();
 
   // Set the network port
-  const port = process.env.PORT || 8082;
+  const port = process.env.PORT || 8081;
 
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
-
-  app.get("/filteredimage", async (req, res) => {
+  app.get("/filteredimage", async (req : Request, res: Response) => {
     const { image_url } = req.query;
-    if (image_url && validUrl.isUri(image_url)) {
-      const filteredpath = await filterImageFromURL(image_url);
-      res.sendFile(filteredpath);
-      deleteLocalFiles([filteredpath]);
+    const tempFiles : string[] = []
+    if (image_url && validUrl.isUri(image_url))
+    {
+      try
+      {
+        let filteredpath: string = await filterImageFromURL(image_url);
+        tempFiles.push(filteredpath);
+        res.status(200).sendFile(filteredpath, (err: Error) =>
+        {
+          if (err)
+          {
+            res.status(500).send({message: "Something went wrong"})
+          }
+          deleteLocalFiles(tempFiles);
+          
+        });
+        
+        
+      } catch (err)
+      {
+        console.log(err)
+        res.status(500).send({ message: "Something went wrong" });
+      } 
     } else {
-      res.send("try GET /filteredimage?image_url={{}} with a valid image_url");
+      res.status(422).send({message: "image_url is required"});
     }
   });
   //! END @TODO1
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get("/", async (req, res) => {
+  app.get("/", async (req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}");
   });
 
--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -11,7 +11,7 @@ import Jimp = require('jimp');
 export async function filterImageFromURL(inputURL: string): Promise<string>{
     return new Promise( async resolve => {
         const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
         await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
