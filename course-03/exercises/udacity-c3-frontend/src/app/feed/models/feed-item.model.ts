58c60d553244786824f746c29f95931d85f76d23
Rachel Tumulak <rmktumulak@gmail.com>
2020-03-14 04:47:07 +0800

    add course 3 exercises

---
+++ course-03/exercises/udacity-c3-frontend/src/app/feed/models/feed-item.model.ts
@@ -0,0 +1,23 @@
+export interface FeedItem {
+    id: number;
+    url: string;
+    caption: string;
+}
+
+export const feedItemMocks: FeedItem[] = [
+    {
+    id: 0,
+    url: '/assets/mock/xander0.jpg',
+    caption: 'Such a cute pup'
+    },
+    {
+    id: 0,
+    url: '/assets/mock/xander1.jpg',
+    caption: 'Who\'s a good boy?'
+    },
+    {
+    id: 0,
+    url: '/assets/mock/xander2.jpg',
+    caption: 'Majestic.'
+    }
+];
