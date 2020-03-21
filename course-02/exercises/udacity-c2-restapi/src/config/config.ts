94638c18567750c24ec527867b28d9e17f7481d5
Nakita <nakita.strangeways@numerator.com>
2020-03-21 14:37:49 -0700

    adding user and pass to config file

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: "udagramnakitadev",
+    password: "wb%3MD][hx=2",
+    database: "udagramnstrange",
+    host: "udagramnstrange.cj6xemhpolby.us-west-1.rds.amazonaws.com",
+    dialect: "postgres",
+    aws_region: "us-west-1",
+    aws_profile: "default",
+    aws_media_bucket: "udagram-ruttner-dev"
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres"
   }
-}
+};
