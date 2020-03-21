f7252bb47e40ad3e138fcc56df6bc8283829a4c1
Danilo Rossi <contact@danilorossi.me>
2020-03-21 12:49:57 +0100

    feat: Implement image filter endpoint - wip

--- course-02/project/image-filter-starter-code/src/imageFilters.route.ts
+++ course-02/project/image-filter-starter-code/src/imageFilters.route.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from 'express';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
 const router: Router = Router();
 
 
@@ -20,8 +22,28 @@ const router: Router = Router();
   /**************************************************************************** */
 
   //! END @TODO1
-router.get('/', async (req: Request, res: Response) => {    
-    res.send(`Working`);
+router.get('/', async (req: Request, res: Response) => {   
+
+  const image_url:string = req.query.image_url;
+
+  // 1. validate the image_url query
+  if(!image_url || image_url.length <= 0) {
+    res.status(400).send({ message: 'Missing image_url query string parameter, or wrong parameter value'});
+  }
+
+  // 2. call filterImageFromURL(image_url) to filter the image
+  let filteredPath:string = null;
+  try {
+    filteredPath = await filterImageFromURL(image_url);
+  } catch (error) {
+    res.status(500).send({ message: 'An error ocurred', error});
+  }
+  
+  // 3. send the resulting file in the response
+  res.status(200).sendFile(filteredPath);
+
+  // 4. deletes any files on the server on finish of the response
+  // TODO
 });
 
 
--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
 import { ImageFiltersRouter } from './imageFilters.route';
 (async () => {
 
