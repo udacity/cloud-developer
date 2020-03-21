66f4baaa281745e84d3a47ddda18d5ce0ea4bb05
Danilo Rossi <contact@danilorossi.me>
2020-03-21 12:25:19 +0100

    feat: Add project endpoint scaffolding

---
+++ course-02/project/image-filter-starter-code/src/imageFilters.route.ts
@@ -0,0 +1,28 @@
+import { Router, Request, Response } from 'express';
+const router: Router = Router();
+
+
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+router.get('/', async (req: Request, res: Response) => {    
+    res.send(`Working`);
+});
+
+
+export const ImageFiltersRouter: Router = router;
