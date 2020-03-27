38319db56159d8a38159e2a804085a8842482835
Ulhas Pai <upai@kayak.com>
2020-03-27 08:18:42 -0400

    exercise updates to rest api

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,21 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+    'dev': {
+        'username': process.env.UDAGRAM_USERNAME,
+        'password': process.env.UDAGRAM_PASSWORD,
+        'database': process.env.UDAGRAM_DATABASE,
+        'host': process.env.UDAGRAM_DATABASE_HOST,
+        'aws_media_bucket': process.env.UDAGRAM_S3_BUCKET,
+        'dialect': 'postgres',
+        'aws_profile': process.env.AWS_PROFILE
+    },
+    'prod': {
+        'username': process.env.UDAGRAM_USERNAME,
+        'password': process.env.UDAGRAM_PASSWORD,
+        'database': process.env.UDAGRAM_DATABASE,
+        'host': process.env.UDAGRAM_DATABASE_HOST,
+        'dialect': 'postgres'
+    },
+    'jwt': {
+        'secret' : 'helloworld'
+    }
+};
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -7,100 +7,101 @@ import * as jwt from 'jsonwebtoken';
 import { NextFunction } from 'connect';
 
 import * as EmailValidator from 'email-validator';
+import { config } from '../../../../config/config';
 
 const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-    //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+    const rounds = 10;
+    const salt = await bcrypt.genSalt(rounds);
+    return bcrypt.hash(plainTextPassword, salt);
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
-    //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
+    return await bcrypt.compare(plainTextPassword, hash);
 }
 
 function generateJWT(user: User): string {
-    //@TODO Use jwt to create a new JWT Payload containing
+    return jwt.sign(user, config.jwt.secret);
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
-    return next();
-    // if (!req.headers || !req.headers.authorization){
-    //     return res.status(401).send({ message: 'No authorization headers.' });
-    // }
-    
+    if (!req.headers || !req.headers.authorization) {
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
 
-    // const token_bearer = req.headers.authorization.split(' ');
-    // if(token_bearer.length != 2){
-    //     return res.status(401).send({ message: 'Malformed token.' });
-    // }
-    
-    // const token = token_bearer[1];
+    const token_bearer = req.headers.authorization.split(' ');
+    if (token_bearer.length !== 2) {
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
 
-    // return jwt.verify(token, "hello", (err, decoded) => {
-    //   if (err) {
-    //     return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-    //   }
-    //   return next();
-    // });
+    const token = token_bearer[1];
+
+    return jwt.verify(token, config.jwt.secret, (err, decoded) => {
+        if (err) {
+            return res.status(500).send({auth: false, message: 'Failed to authenticate.'});
+        }
+        return next();
+    });
 }
 
-router.get('/verification', 
-    requireAuth, 
+router.get('/verification',
+    requireAuth,
     async (req: Request, res: Response) => {
-        return res.status(200).send({ auth: true, message: 'Authenticated.' });
-});
+        return res.status(200).send({auth: true, message: 'Authenticated.'});
+    });
 
 router.post('/login', async (req: Request, res: Response) => {
     const email = req.body.email;
     const password = req.body.password;
     // check email is valid
     if (!email || !EmailValidator.validate(email)) {
-        return res.status(400).send({ auth: false, message: 'Email is required or malformed' });
+        return res.status(400).send({auth: false, message: 'Email is required or malformed'});
     }
 
     // check email password valid
     if (!password) {
-        return res.status(400).send({ auth: false, message: 'Password is required' });
+        return res.status(400).send({auth: false, message: 'Password is required'});
     }
 
     const user = await User.findByPk(email);
     // check that user exists
-    if(!user) {
-        return res.status(401).send({ auth: false, message: 'Unauthorized' });
+    if (!user) {
+        return res.status(401).send({auth: false, message: 'Unauthorized'});
     }
 
     // check that the password matches
-    const authValid = await comparePasswords(password, user.password_hash)
+    const authValid = await comparePasswords(password, user.password_hash);
 
-    if(!authValid) {
-        return res.status(401).send({ auth: false, message: 'Unauthorized' });
+    if (!authValid) {
+        return res.status(401).send({auth: false, message: 'Unauthorized'});
     }
 
     // Generate JWT
-    const jwt = generateJWT(user);
+    const generatedJWT = generateJWT(user);
 
-    res.status(200).send({ auth: true, token: jwt, user: user.short()});
+    res.status(200).send({auth: true, token: generatedJWT, user: user.short()});
 });
 
-//register a new user
+// register a new user
 router.post('/', async (req: Request, res: Response) => {
     const email = req.body.email;
     const plainTextPassword = req.body.password;
     // check email is valid
     if (!email || !EmailValidator.validate(email)) {
-        return res.status(400).send({ auth: false, message: 'Email is required or malformed' });
+        return res.status(400).send({auth: false, message: 'Email is required or malformed'});
     }
 
     // check email password valid
     if (!plainTextPassword) {
-        return res.status(400).send({ auth: false, message: 'Password is required' });
+        return res.status(400).send({auth: false, message: 'Password is required'});
     }
 
     // find the user
     const user = await User.findByPk(email);
     // check that user doesnt exists
-    if(user) {
-        return res.status(422).send({ auth: false, message: 'User may already exist' });
+    if (user) {
+        return res.status(422).send({auth: false, message: 'User may already exist'});
     }
 
     const password_hash = await generatePassword(plainTextPassword);
@@ -118,13 +119,13 @@ router.post('/', async (req: Request, res: Response) => {
     }
 
     // Generate JWT
-    const jwt = generateJWT(savedUser);
+    const generatedJWT = generateJWT(savedUser);
 
-    res.status(201).send({token: jwt, user: savedUser.short()});
+    res.status(201).send({token: generatedJWT, user: savedUser.short()});
 });
 
 router.get('/', async (req: Request, res: Response) => {
-    res.send('auth')
+    res.send('auth');
 });
 
 export const AuthRouter: Router = router;
