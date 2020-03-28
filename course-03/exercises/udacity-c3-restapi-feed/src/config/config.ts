cafeaea2ce275d16e58de05900eff1b6aab1fa09
Thomas Weber <twebe@outlook.de>
2020-03-28 12:43:09 +0100

    Added starter code for course 3

---
+++ course-03/exercises/udacity-c3-restapi-feed/src/config/config.ts
@@ -0,0 +1,24 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DB,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET,
+    "url": process.env.URL    
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
+  }
+
+}
