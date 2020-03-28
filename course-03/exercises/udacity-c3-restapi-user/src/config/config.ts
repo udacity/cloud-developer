7ded6ac9ed564751a77b17cfd39afc5a37cd2571
Thomas Weber <twebe@outlook.de>
2020-03-28 13:30:27 +0100

    Added CI support and updated environment variables

--- course-03/exercises/udacity-c3-restapi-user/src/config/config.ts
+++ course-03/exercises/udacity-c3-restapi-user/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
-    "host": process.env.POSTGRESS_HOST,
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
     "aws_reigion": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
     "url": process.env.URL
   },
   "prod": {
