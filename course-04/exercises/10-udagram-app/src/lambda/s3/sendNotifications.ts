69796985d3d5b58f90ebcc38aae3de0630ee531a
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-04-04 17:11:31 -0300

    Adicionando sistema de notificacoes e elastic search

--- course-04/exercises/10-udagram-app/src/lambda/s3/sendNotifications.ts
+++ course-04/exercises/10-udagram-app/src/lambda/s3/sendNotifications.ts
@@ -1,6 +1,6 @@
-import { SNSHandler, SNSEvent, S3Event, S3Handler } from 'aws-lambda'
+import { SNSHandler, SNSEvent, S3Event } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
+import * as AWS  from 'aws-sdk'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
@@ -15,25 +15,34 @@ const connectionParams = {
 
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
 
-export const handler: S3Handler = async (event: S3Event) => {
-  for (const record of event.Records) {
+export const handler: SNSHandler = async (event: SNSEvent) => {
+  console.log('Processing SNS event ', JSON.stringify(event))
+  for (const snsRecord of event.Records) {
+    const s3EventStr = snsRecord.Sns.Message
+    console.log('Processing S3 event', s3EventStr)
+    const s3Event = JSON.parse(s3EventStr)
+
+    await processS3Event(s3Event)
+  }
+}
+
+async function processS3Event(s3Event: S3Event) {
+  for (const record of s3Event.Records) {
     const key = record.s3.object.key
-    console.log('Processing S3 item with key: ' + key);
+    console.log('Processing S3 item with key: ', key)
 
     const connections = await docClient.scan({
-      TableName: connectionsTable
+        TableName: connectionsTable
     }).promise()
 
     const payload = {
-      imageId: key
+        imageId: key
     }
 
     for (const connection of connections.Items) {
-      const connectionId = connection.id
-      await sendMessageToClient(connectionId, payload)
+        const connectionId = connection.id
+        await sendMessageToClient(connectionId, payload)
     }
-
-
   }
 }
 
