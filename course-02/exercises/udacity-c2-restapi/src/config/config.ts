a4849a1a9a581584cccf27ac40e381d316ff70f3
Steven Snyder <stevensnyder@stevens-mbp.attlocal.net>
2020-04-03 21:54:27 -0400

    updated feed router, config, aws and authrouter files

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "username": process.env.username,
+    "password": process.env.password,
+    "database": process.env.database,
+    "host": process.env.host,
+    "dialect": process.env.dialect,
+    "aws_region": process.env.aws_region,
+    "aws_profile": process.env.aws_profile,
+    "aws_media_bucket": process.env.aws_media_bucket
   },
   "prod": {
     "username": "",
