4e8d19cc84bde9f5939edf8ad811429817f48ea9
Andrew Almaguer <andrew@almaguer.dev>
2020-04-05 11:21:29 +0200

    Add new endpoint with image_url query check

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -30,6 +30,14 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+  app.get( "/filteredimage/", async ( req, res ) => {
+    let { image_url } = req.query;
+
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`image_url is required`);
+    }
+  })
   
   // Root Endpoint
   // Displays a simple message to the user
