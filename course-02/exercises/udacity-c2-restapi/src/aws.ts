8f42c9eef8f7a9580aa01ee0adbd5d1c38df848a
sunilkrishnatry <Getthegit1>
2020-04-07 21:03:17 -0500

    Added changes for using environment variables

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -2,15 +2,18 @@ import AWS = require('aws-sdk');
 import { config } from './config/config';
 
 const c = config.postgres;
+const aws = config.aws;
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.credentials = credentials;
+if(aws.aws_profile !== "DEPLOYED") {
+  var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+  AWS.config.credentials = credentials;
+}
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
-  region: c.aws_region,
-  params: {Bucket: c.aws_media_bucket}
+  region: aws.aws_region,
+  params: {Bucket: aws.aws_media_bucket}
 });
 
 
@@ -25,7 +28,7 @@ export function getGetSignedUrl( key: string ): string{
   const signedUrlExpireSeconds = 60 * 5
 
     const url = s3.getSignedUrl('getObject', {
-        Bucket: c.aws_media_bucket,
+        Bucket: aws.aws_media_bucket,
         Key: key,
         Expires: signedUrlExpireSeconds
       });
@@ -44,7 +47,7 @@ export function getPutSignedUrl( key: string ){
     const signedUrlExpireSeconds = 60 * 5
 
     const url = s3.getSignedUrl('putObject', {
-      Bucket: c.aws_media_bucket,
+      Bucket: aws.aws_media_bucket,
       Key: key,
       Expires: signedUrlExpireSeconds
     });
--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,20 +1,15 @@
 export const config = {
   "postgres": {
-    "username": "postgres",
-    "password": "postgres1",
-    "database": "udgramdatabse",
-    "host": "database1.cmtn29uv7ozo.us-east-1.rds.amazonaws.com",
-    "dialect": "postgres",
-    "aws_region": "us-east-1",
-    //"aws_profile": "default",
-    "aws_media_bucket": "krishnatry-website",
-    "storage":"memory"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres"
+  },
+  "aws": {
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
+    "storage":"memory"
   }
 }
