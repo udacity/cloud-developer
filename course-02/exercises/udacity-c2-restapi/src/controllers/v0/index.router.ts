20134a7a317148f713d78576a939662b538c3e8d
David Kerekes <david.kerekes@dbx.hu>
2020-04-10 22:30:19 +0200

    changes to controllers and server.ts

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/index.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/index.router.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
-import { FeedRouter } from './feed/routes/feed.router';
-import { UserRouter } from './users/routes/user.router';
+import { FeedRouter }                from './feed/routes/feed.router';
+import { UserRouter }                from './users/routes/user.router';
+
 
 const router: Router = Router();
 
@@ -8,7 +9,7 @@ router.use('/feed', FeedRouter);
 router.use('/users', UserRouter);
 
 router.get('/', async (req: Request, res: Response) => {    
-    res.send(`V0`);
+  res.send(`V0`);
 });
 
 export const IndexRouter: Router = router;
