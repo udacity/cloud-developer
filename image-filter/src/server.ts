b233b58e09f4ad6cd1e638ee0ab452913dc9de48
David Kerekes <david.kerekes@dbx.hu>
2020-04-12 20:54:11 +0200

    filteredImage api endpoint added

--- image-filter/src/server.ts
+++ image-filter/src/server.ts
@@ -1,6 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express, { Request, Response }           from 'express';
+import bodyParser                               from 'body-parser';
+import fs                                       from 'fs';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+
 
 (async () => {
 
@@ -17,7 +19,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -26,21 +27,29 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
   /**************************************************************************** */
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    const { image_url } = req.query;
+    const imageDir: string = '/util/tmp/';
+    const picturePath: string = await filterImageFromURL(image_url);
+    // 'filterImageFromURL' also validates the URL with the help of 'Jimp'
+    const noPic: string = 'No picture at this address (or wrong address has been provided)!';
+    if (picturePath === 'Not valid') return res.status(422).send(noPic);
+    fs.readdir(__dirname+imageDir, (err, files: Array<string>) => deleteLocalFiles(files));
+  });
 
   //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
 
   // Start the Server
   app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
+      console.log(`server running http://localhost:${ port }` );
+      console.log(`press CTRL+C to stop server`);
   } );
 })();
