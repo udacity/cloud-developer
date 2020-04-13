5c02da8ff63e4ee24895a94e647a32d78eab24a4
wjclemens69 <62310455+wjclemens69@users.noreply.github.com>
2020-04-13 00:47:29 -0400

    Adding Project 2 Work

---
+++ course-02/project/image-filter-starter-code/src/controllers/v0/feed/routes/imagefeed.router.ts
@@ -0,0 +1,52 @@
+﻿import { Router, Request, Response } from 'express';
+import Index = require("@types/express/index");
+import Util = require("../../util/util");
+//import filterImageFromURL = Utils.filterImageFromURL;
+
+
+
+const router: Router = Router();
+
+router.get('/', async (req: Request, res: Response) => {
+    
+    res.status(200).send("imagefeed.router");
+});
+
+//http://localhost:8082/api/v0/feed/imagefilter/filteredImage?image_url=C:\Clemens\Personal\DW\dw.jpg
+router.get('/imagefilter', async (req: Request, res: Response) => {
+    image_url: String;
+
+    let { image_url } = req.query;
+   // res.send(image_url);
+    if (image_url) {
+
+      let imglength = image_url.length;
+    let fileExtension = image_url.substring(imglength - 4);
+
+    if (fileExtension != ".jpg") {
+
+      res.status(422).send("File must be .jpg type");
+    }
+
+    res.on('finish', function () {
+     Util.deleteLocalFiles([filteredpath]);
+    });
+
+    const filteredpath = await Util.filterImageFromURL(image_url);
+   
+    res.status(200).sendFile(filteredpath);
+
+    }
+    else {
+      res.status(400).send("image_url not specified");
+
+    }
+
+});
+
+
+
+export const ImageFeedRouter: Router = router;
+
+
+
--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,41 +1,57 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { ImageRouter } from './controllers/v0/image.router';
+
 
 (async () => {
 
-  // Init the Express application
-  const app = express();
+    // Init the Express application
+    const app = express();
 
-  // Set the network port
-  const port = process.env.PORT || 8082;
+    // Set the network port
+    const port = process.env.PORT || 8082;
+
+    // Use the body parser middleware for post requests
+    app.use(bodyParser.json());
+
+    //CORS Should be restricted
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        next();
+    });
+
+    
+    
+    
+
+    ;
+    app.use('/api/v0/',ImageRouter);
   
-  // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+      // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+      // GET /filteredimage?image_url={{URL}}
+      // endpoint to filter an image from a public url.
+      // IT SHOULD
+      //    1
+      //    1. validate the image_url query
+      //    2. call filterImageFromURL(image_url) to filter the image
+      //    3. send the resulting file in the response
+      //    4. deletes any files on the server on finish of the response
+      // QUERY PARAMATERS
+      //    image_url: URL of a publicly accessible image
+      // RETURNS
+      //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
+      /**************************************************************************** */
 
   //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
+  //app.get( "/", async ( req, res ) => {
+    //res.send("try GET /filteredimage?image_url={{}}")
+  //} );
   
 
   // Start the Server
