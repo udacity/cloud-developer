9ec14fd68b28f796a618dd0e684c20c1ecd71073
Steven Duncan-Brown <stevenduncanbrown@gmail.com>
2020-04-22 19:05:53 +0100

    Make JWT secret an env variable

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -9,7 +9,7 @@ export const config = {
     aws_profile: process.env.AWS_PROFILE,
     aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
     jwt: {
-      secret: "helloworld",
+      secret: process.env.JWT_SECRET,
     },
   },
   prod: {
