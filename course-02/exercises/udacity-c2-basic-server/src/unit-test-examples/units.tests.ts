f71342c1534e3ccf9ecbce06978ba1dddc31250c
Jose Mendez <jomendezdev@gmail.com>
2020-04-27 10:59:43 -0400

    server unit test

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,14 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+
+describe('concat', () => {
+
+  it('should concat two strings', () => {
+    const result = concat('hello','world');
+    expect(result).to.equal('helloworld');
+  });
+
+ 
+
+});
