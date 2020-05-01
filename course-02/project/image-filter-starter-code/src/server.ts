8b226082f7b82f9727db7b63911d53d791b51865
dileque <38050481+dileque@users.noreply.github.com>
2020-05-01 19:00:42 +0300

    image-filter-project
    image-filter-project submission changes

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -29,6 +29,20 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get("/filteredimage", async (req, res) => {
+    let image_url = req.query.image_url;
+    if (!image_url)
+      res.status(400).send("Image url is not correct!");
+    else {
+      filterImageFromURL(image_url).then((response) => {
+        res.sendFile(response);
+        res.on('finish', function() {
+          deleteLocalFiles([response]);
+        });
+      });
+    }
+  });
+
   //! END @TODO1
   
   // Root Endpoint
