0a5e8b58ce1a9372ad0093ed51d166e34f50570c
exricel <exricel@gmail.com>
2020-05-03 01:21:47 +0200

    1st implementation of filteredimage

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -13,24 +13,34 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  // Filter image from query param
+  app.get("/filteredimage",
+    async (req: Request, res: Response) => {
+      let {image_url} = req.query;
 
-  /**************************************************************************** */
+      if (!image_url) {
+        return res.status(400)
+          .send(`image_url is required`);
+      }
 
-  //! END @TODO1
-  
+      await filterImageFromURL(image_url)
+        .then(async filteredImagePath =>
+          res.sendFile(filteredImagePath, function (err) {
+            if (err) {
+              console.log('Error on transfer', err)
+            } else {
+              console.log('Sent:', filteredImagePath);
+            }
+            //we want to delete tmp file even if there is an error during send
+            deleteLocalFiles([filteredImagePath]);
+          }))
+        .catch((err) => {
+          console.log(err);
+          res.status(422)
+            .send(`not able to process the specified image`);
+        });
+    });
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -8,17 +8,20 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+        const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+        await Jimp.read(inputURL)
+          .then(photo => {
+              photo
+                .resize(256, 256) // resize
+                .quality(60) // set JPEG quality
+                .greyscale() // set greyscale
+                .write(__dirname + outpath, (img) => {
+                    resolve(__dirname + outpath);
+                });
+          })
+          .catch(err => reject(err));
     });
 }
 
