3ef526282f63640f2ebe30abf840773632e25deb
André Brinkop <andre@brinkop.de>
2020-05-04 11:33:29 +0000

    add GET /filteredimage endpoint that takes an image url and returns a filtered version of that image

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,23 +13,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
+  app.get("/filteredimage", async ( req, res, next ) => {
+    const { image_url } = req.query;
+    
+    if (!image_url) {
+      return res.status(400).send("image_url is required");
+    }
+    
+    var filteredImagePath: string;
+    try {
+      filteredImagePath = await filterImageFromURL(image_url);
+    } catch {
+      return res.status(422).send("invalid image_url");
+    }
+    
+    res.status(200).sendFile(filteredImagePath, err => {
+      deleteLocalFiles([filteredImagePath]);
+      next(err);
+    });
+    
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
