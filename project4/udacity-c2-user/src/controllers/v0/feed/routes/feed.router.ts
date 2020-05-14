425c355db4cb1e04b06ab4a860c76122a52dfa50
Mohamadou Ly <mohamadou.ly@hpe.com>
2020-05-13 22:05:57 -0400

    feat: Copy and Rename restapi project
    Made 2 copies of the restapi project and named the 2 new project respectively
    udacity-c2-feed and udacity-c2-user.
    
    udacity-c2-feed: project for handling /feed endpoint
    udacity-c2-users: project for handling /users endpoint

---
+++ project4/udacity-c2-user/src/controllers/v0/feed/routes/feed.router.ts
@@ -0,0 +1,118 @@
+import { Router, Request, Response } from 'express';
+import { FeedItem } from '../models/FeedItem';
+import { requireAuth } from '../../users/routes/auth.router';
+import * as AWS from '../../../../aws';
+
+const router: Router = Router();
+
+// Get all feed items
+router.get('/', async (req: Request, res: Response) => {
+    const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
+    items.rows.map((item) => {
+            if(item.url) {
+                item.url = AWS.getGetSignedUrl(item.url);
+            }
+    });
+    res.send(items);
+});
+
+//@TODO
+//Add an endpoint to GET a specific resource by Primary Key
+router.get("/:id",
+    requireAuth,
+    async (req: Request, res: Response) => {
+        let { id } = req.params
+
+        if ( !id ) {
+            res.status(400).send(`resource id is required`)
+        }
+
+        const feed = await FeedItem.findByPk(id)
+
+        if ( !feed ) {
+            res.status(404).send(`Feed with ${id} not found in the DB`)
+        } else {
+            res.status(200).send(feed)
+        }
+})
+// update a specific resource
+router.patch('/:id', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+        //@TODO try it yourself
+        //res.status(500).send("not implemented")
+
+        const { id } = req.params
+        //const caption = req.body.caption
+        const caption = req.body.caption
+        const fileName = req.body.url
+
+        if ( !id ) {
+            res.status(400).send({message: 'resource id is required'})
+        }
+        if ( !caption ) {
+            res.status(400).send({message: 'caption is required or malformed'})
+        }
+
+        if ( !fileName ) {
+            res.status(400).send({message: 'file url is require'})
+        }
+       
+        const feed = await FeedItem.findByPk(id)
+
+        if ( !feed ) {
+            res.status(404).send(`Couldn't find feed with ${id}`)
+        }
+
+        const [updateRows, updatedFeed] = await FeedItem.update( 
+            { caption: caption, url: fileName},
+            { returning: true, where: {id: id}}
+        )
+
+        const saved_url = AWS.getGetSignedUrl(updatedFeed[0].url)
+
+        res.status(201).send("Successfully updated, " + saved_url)
+
+});
+
+
+// Get a signed url to put a new item in the bucket
+router.get('/signed-url/:fileName', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+    let { fileName } = req.params;
+    const url = AWS.getPutSignedUrl(fileName);
+    res.status(201).send({url: url});
+});
+
+// Post meta data and the filename after a file is uploaded 
+// NOTE the file name is they key name in the s3 bucket.
+// body : {caption: string, fileName: string};
+router.post('/', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+    const caption = req.body.caption;
+    const fileName = req.body.url;
+
+    // check Caption is valid
+    if (!caption) {
+        return res.status(400).send({ message: 'Caption is required or malformed' });
+    }
+
+    // check Filename is valid
+    if (!fileName) {
+        return res.status(400).send({ message: 'File url is required' });
+    }
+
+    const item = await new FeedItem({
+            caption: caption,
+            url: fileName
+    });
+
+    const saved_item = await item.save();
+
+    saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+    res.status(201).send(saved_item);
+});
+
+export const FeedRouter: Router = router;
