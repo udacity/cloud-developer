54107cfb6de04d1e6bd3bdfa5696e6cf0d0760ad
akryvorutsky3 <alexander.kryvorutsky@gmail.com>
2020-05-13 23:38:30 -0400

    implemented filterImageFromURL

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -17,7 +17,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -26,9 +25,34 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
   /**************************************************************************** */
+  app.get( "/filteredimage", async ( req, res ) => {
+    //const {image_url} = req.query.image_url;
+    let {image_url} = req.query;
 
+    console.log('Got image_url: ${image_url}');
+
+    if(!image_url) {
+      res.status(400).send('image_url parameter is missing');
+    }  
+    
+    let image_file:string;
+
+    //handle bad url parameter:
+    try {
+      image_file = await filterImageFromURL(image_url);
+    } catch(error) {
+      res.status(500).send('Error on filterImageFromURL:' + error);
+    }
+
+    console.log('Got image file url: ${image_file}');
+
+    res.sendFile(image_file, () => {
+      console.log('Before delete local file ${image_file}');
+      deleteLocalFiles([image_file]).catch(error => {console.error(error)});
+      console.log('After delete local file ${image_file}');
+    })
+  });
   //! END @TODO1
   
   // Root Endpoint
