af06598203336f7b3ba316caddb645b215cf0464
noracode <noracode@users.noreply.github.com>
2020-05-17 11:06:50 -0700

    Updates restapi exercise

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -4,7 +4,7 @@ import { config } from './config/config';
 const c = config.dev;
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+var credentials = new AWS.SharedIniFileCredentials({ profile: c.aws_profile });
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
@@ -20,17 +20,17 @@ export const s3 = new AWS.S3({
  * @Returns:
  *    a url as a string
  */
-export function getGetSignedUrl( key: string ): string{
+export function getGetSignedUrl( key: string ): string {
 
   const signedUrlExpireSeconds = 60 * 5
 
-    const url = s3.getSignedUrl('getObject', {
-        Bucket: c.aws_media_bucket,
-        Key: key,
-        Expires: signedUrlExpireSeconds
-      });
+  const url = s3.getSignedUrl('getObject', {
+      Bucket: c.aws_media_bucket,
+      Key: key,
+      Expires: signedUrlExpireSeconds
+    });
 
-    return url;
+  return url;
 }
 
 /* getPutSignedUrl generates an aws signed url to put an item
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -16,15 +16,33 @@ router.get('/', async (req: Request, res: Response) => {
     res.send(items);
 });
 
+
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const item = await FeedItem.findByPk(id);
+    if (item === null) {
+        res.status(404).send('item not found')
+    } else {
+        res.send(item);
+    }
+});
+
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        const { id } = req.params;
+        const { caption, url } = req.body;
+        await FeedItem.update({ caption: caption, url: url }, {
+            where: {
+                id: id
+            }
+        })
+        res.send(200).send("updated")
 });
 
 
