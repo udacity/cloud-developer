144d0b2858d026e30354558e18ba375982d047b6
Arunkumar Chacko <acchack@amazon.com>
2020-05-18 19:55:33 +0530

    Remove commented code

--- course-02/project/image-filter-starter-code/src/controllers/filterimage.router.ts
+++ course-02/project/image-filter-starter-code/src/controllers/filterimage.router.ts
@@ -5,12 +5,11 @@ import {BAD_REQUEST,OK, INTERNAL_SERVER_ERROR, NOT_FOUND} from 'http-status-code
 const router: Router = Router();
 
 router.get('/', async (req: Request, res: Response) => {    
-    console.log("filterimage called")
     const imageUrl = req.query.image_url
-    console.log(imageUrl)
 
+    console.log(`filterimage called for ${imageUrl}`)
     if(!imageUrl) {
-        return res.status(BAD_REQUEST).send(`the URL '${imageUrl}' is not in the expected format`)
+        return res.status(BAD_REQUEST).send(`the image URL cannot be empty`)
     }
 
     try {
@@ -21,36 +20,12 @@ router.get('/', async (req: Request, res: Response) => {
 
     try {
         const tempFileName =  await filterImageFromURL(imageUrl)
-        // res.sendFile(tempFileName)
         res.sendFile(tempFileName, (_) => {deleteLocalFiles([tempFileName])})
-
-        try {
-            // deleteLocalFiles([tempFileName])
-        }
-        catch(TypeError) {
-            console.error(`deleting ${tempFileName} failed`)
-            //ignore
-        }
         return 
     }
     catch(TypeError) {
         return res.status(NOT_FOUND).send("Not found")
-        
     }
-    // one.then(value => {
-    //     console.log('resolved', value);
-    //     res.sendFile(value, (err) => {deleteLocalFiles([value])})
-    //   }); // TODO: Move to finally
-    //   one.catch(error => {
-    //     console.log('rejected', error);
-    //     res.status(NOT_FOUND).send(error)
-    //   });
-
-      
-
-      
-
-    // res.status(OK).send(`OK`);
 });
 
 export const FilterImageRouter: Router = router;
