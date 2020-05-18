14b3c91506ff202fae05beff7870e4b2fb4a074e
Mohamadou Ly <mohamadou.ly@hpe.com>
2020-05-18 09:11:21 -0400

    feat: Create a build script
    Created a build script for building the docker images of the
    3 microservices(user, feed, and reverse-proxy)

---
+++ project4/build_script.sh
@@ -0,0 +1,3 @@
+docker build -t mamadly/udacity-cloud-dev:reverse-proxy-v1 reverse-proxy 
+docker build -t mamadly/udacity-cloud-dev:feed-v1 udacity-c2-frontend
+docker build -t mamadly/udacity-cloud-dev:feed-v1 udacity-c2-feed
