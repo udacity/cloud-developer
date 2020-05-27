7351677b0e62105649fdd96723965b3f02cfbc76
mgmacdougall <MacD>
2020-05-26 20:32:39 -0400

    Add of config.ts

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,20 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+	dev: {
+		username: 'udagammacddev2',
+		password: 'udagammacddev2',
+		database: 'postgres',
+		host: 'udagammacddev2.co6vfctjgigk.ca-central-1.rds.amazonaws.com',
+		dialect: 'postgres',
+		storage: ':memory:',
+		aws_region: 'ca-central-1',
+		aws_profile: 'default',
+		aws_media_bucket: 'udagammacddev2',
+	},
+	prod: {
+		username: '',
+		password: '',
+		database: 'udagram_prod',
+		host: '',
+		dialect: 'postgres',
+	},
+};
