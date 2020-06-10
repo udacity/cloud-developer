93822830f4e387f191aee8082664993ff599ff08
YASWANTH SAI <35864576+Yaswanth899@users.noreply.github.com>
2020-06-10 18:45:03 +0530

    installed dependices

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -70,7 +70,15 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
-
+    app.get("/cars/",
+        async(req:Request,res:Response)=>{
+          const {cars_list}= req.params;
+          if(!cars_list){
+              return res.status(400).send(`list is empty`);
+          }
+            return res.status(200)
+                      .send(`cars are : ${cars_list}`);
+        });
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
