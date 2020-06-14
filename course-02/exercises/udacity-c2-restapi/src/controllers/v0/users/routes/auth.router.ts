005af5174117f626361c462bce331e4202d921ef
Guru Kuppuswamy <guruprasad.npk@gmail.com>
2020-06-13 17:52:40 -0700

    completed methods for authentication

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -12,12 +12,16 @@ const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
     //@TODO Use Bcrypt to Generated Salted Hashed Passwords
-    return new Promise(() => { return "test" });
+
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(plainTextPassword, salt);
+    return hash;
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-    return new Promise(() => { return true });
+    return await bcrypt.compare(plainTextPassword, hash); 
 }
 
 function generateJWT(user: User): string {
