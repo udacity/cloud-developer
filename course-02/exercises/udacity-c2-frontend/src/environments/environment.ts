688fab26d59ff2b4d62c3fc4f8a4a8e6d0c405a5
Ziv Jia <zhiyuan.jia@uber.com>
2020-06-14 00:23:06 -0400

    filter image

--- course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
+++ course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagram-jia-dev-dev.us-east-1.elasticbeanstalk.com/api/v0/'
 };
 
 /*
--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import fs from 'fs';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,7 +10,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +31,33 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get("/filteredimage", async (req, res) => {
+    const { image_url: imageUrl } = req.query;
+    if (!imageUrl) {
+      return res.status(400).send('Image url is required');
+    }
+    res.sendFile(await filterImageFromURL(imageUrl), {}, async () => {
+      let picDirs: Array<string> = [];
+      picDirs = await fs.readdirSync(__dirname + '/util/tmp');
+      picDirs = picDirs.map(function (file) {
+        // Do whatever you want to do with the file
+        return __dirname + '/util/tmp/' + file;
+      });
+      await deleteLocalFiles(picDirs);
+    });
+  });
+
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
