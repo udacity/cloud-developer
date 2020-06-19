6693008788aaaae91c33f1b2da785ef0d7720b77
Kendy <1673979+kendyjm@users.noreply.github.com>
2020-06-19 11:42:43 +0200

    - added support of Sentry to monitor errors etc - externalized configuration of CORS policy in an environnement variable

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -7,9 +7,16 @@ import bodyParser from 'body-parser';
 
 import { V0MODELS } from './controllers/v0/model.index';
 
+import { config } from './config/config';
+
+// Sentry: automatically catch errors and provide a stack trace to developers so we can quickly find and fix bugs
+import * as Sentry from '@sentry/node';
+
 (async () => {
   await sequelize.addModels(V0MODELS);
   await sequelize.sync();
+
+  Sentry.init({ dsn: config.sentry.dsn });
 
   const app = express();
   const port = process.env.PORT || 8080; // default port to listen
@@ -18,7 +25,7 @@ import { V0MODELS } from './controllers/v0/model.index';
 
   //CORS Should be restricted
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Origin", config.cors.access_control_allow_origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
