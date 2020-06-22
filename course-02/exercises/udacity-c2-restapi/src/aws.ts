585fd807ae88cd142dfc408ccc0251e3b19eb9b6
Jean-Christophe Pince <jcpince@gmail.com>
2020-06-22 23:18:11 +0200

    Fix deployment issues.
    Signed-off-by: Jean-Christophe Pince <jcpince@gmail.com>

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -4,8 +4,10 @@ import { config } from './config/config';
 const c = config.dev;
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.credentials = credentials;
+if(c.aws_profile !== "DEPLOYED") {
+  var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
+  AWS.config.credentials = credentials;
+}
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
@@ -24,13 +26,13 @@ export function getGetSignedUrl( key: string ): string{
 
   const signedUrlExpireSeconds = 60 * 5
 
-    const url = s3.getSignedUrl('getObject', {
-        Bucket: c.aws_media_bucket,
-        Key: key,
-        Expires: signedUrlExpireSeconds
-      });
+  const url = s3.getSignedUrl('getObject', {
+      Bucket: c.aws_media_bucket,
+      Key: key,
+      Expires: signedUrlExpireSeconds
+    });
 
-    return url;
+  return url;
 }
 
 /* getPutSignedUrl generates an aws signed url to put an item
@@ -41,13 +43,13 @@ export function getGetSignedUrl( key: string ): string{
  */
 export function getPutSignedUrl( key: string ){
 
-    const signedUrlExpireSeconds = 60 * 5
+  const signedUrlExpireSeconds = 60 * 5
 
-    const url = s3.getSignedUrl('putObject', {
-      Bucket: c.aws_media_bucket,
-      Key: key,
-      Expires: signedUrlExpireSeconds
-    });
+  const url = s3.getSignedUrl('putObject', {
+    Bucket: c.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds
+  });
 
-    return url;
+  return url;
 }
