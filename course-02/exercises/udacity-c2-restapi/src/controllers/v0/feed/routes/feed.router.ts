df826e3056801e0b4b52c6a634439dd20cb4d1c8
Jean-Christophe Pince <jcpince@gmail.com>
2020-06-22 23:15:56 +0200

    Implement exercise restapi
    Signed-off-by: Jean-Christophe Pince <jcpince@gmail.com>

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -8,6 +8,9 @@ const router: Router = Router();
 // Get all feed items
 router.get('/', async (req: Request, res: Response) => {
     const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
+    if (items.count == 0) {
+        return res.status(400).send(`No feed found!!`);
+    }
     items.rows.map((item) => {
             if(item.url) {
                 item.url = AWS.getGetSignedUrl(item.url);
@@ -17,10 +20,10 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 // GET a specific resource by Primary Key
-router.get('/id/', async (req: Request, res: Response) => {
-    let { id } = req.query;
+router.get('/:id', async (req: Request, res: Response) => {
+    let { id } = req.params;
 
-    const item = await FeedItem.findByPk(id, { rejectOnEmpty: true });
+    const item = await FeedItem.findByPk(id);
     if ( ! item || ! item.url ) {
         return res.status(400).send(`id ${id} is not found`);
     }
@@ -31,8 +34,25 @@ router.get('/id/', async (req: Request, res: Response) => {
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
-        //@TODO try it yourself
-        res.send(500).send("not implemented")
+        let { id } = req.params;
+        const caption = req.body.caption;
+        const fileName = req.body.url;
+
+        if ( ! caption || ! fileName ) {
+            return res.status(400).send(`Body incomplete, caption and/or url missing`);
+        }
+
+        const item = await FeedItem.findByPk(id);
+        if ( ! item || ! item.url ) {
+            return res.status(400).send(`id ${id} is not found`);
+        }
+
+        item.caption = caption;
+        item.url = fileName;
+        item.url = AWS.getGetSignedUrl(item.url);
+
+        await item.save();
+        res.status(201).send(item);
 });
 
 
@@ -42,7 +62,7 @@ router.get('/signed-url/:fileName',
     async (req: Request, res: Response) => {
     let { fileName } = req.params;
     const url = AWS.getPutSignedUrl(fileName);
-    res.status(201).send({url: url});
+    res.status(200).send({url: url});
 });
 
 // Post meta data and the filename after a file is uploaded 
