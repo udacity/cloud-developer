b6ed5037e0a72d6ee1bd1507cfe8241d91df0982
Kendy JEAN-MARIE <1673979+kendyjm@users.noreply.github.com>
2020-06-26 15:38:26 +0200

    chore: duplicate udacity-c2-restapi into 2 new projects udacity-c3-restapi-feed and udacity-c3-restapi-user to start seperate the 2 features (feed & user)

---
+++ course-03/exercices/udacity-c3-restapi-feed/src/controllers/v0/model.index.ts
@@ -0,0 +1,4 @@
+import { FeedItem } from './feed/models/FeedItem';
+import { User } from './users/models/User';
+
+export const V0MODELS = [ FeedItem, User ];
---
+++ course-03/exercices/udacity-c3-restapi-user/src/sequelize.ts
@@ -0,0 +1,17 @@
+import {Sequelize} from 'sequelize-typescript';
+import { config } from './config/config';
+
+
+const c = config.db;
+
+// Instantiate new Sequelize instance!
+export const sequelize = new Sequelize({
+  "username": c.username,
+  "password": c.password,
+  "database": c.database,
+  "host":     c.host,
+
+  dialect: 'postgres',
+  storage: ':memory:',
+});
+
