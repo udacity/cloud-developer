bd458b6d932fab0d63e137ebb3b9dc16321202d1
AJ068309 <aditi.jain@cerner.com>
2020-06-26 14:12:36 -0500

    First commit with error

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,18 +1,35 @@
 import express from 'express';
+import { sequelize } from './sequelize';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { IndexRouter } from './controllers/v0/index.router';
+import { V0MODELS } from './controllers/v0/model.index';
 
 (async () => {
-
+  await sequelize.addModels(V0MODELS);
+  await sequelize.sync();
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
+  const files:Array<string> = [];
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+  app.use(function(req, res,next){
+    res.on('close', function(){
+        console.log('the response has been sent');
+        deleteLocalFiles(files).then(()=>{
+          console.log('Files deleted from local folder')
+        }).catch((err)=>{
+          console.log('Error encountered', err);
+        });
+        files.length=0;
+    });
+    next();
+  });
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -33,8 +50,20 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+  app.get( "/filteredimage", async ( req, res ) => {
+    const imageUrl = req.query.image_url;
+    console.log('Requested url:', imageUrl);
+    let filepath='';
+    try{
+      filepath = await filterImageFromURL(imageUrl);
+    }
+    catch(err){
+      console.log(err);
+      res.status(500).send('Error reading image from provided URL');
+    }
+    console.log("FILEPATH: ", filepath);
+    files.push(filepath);
+    res.sendFile(filepath);
   } );
   
 
@@ -43,4 +72,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
       console.log( `server running http://localhost:${ port }` );
       console.log( `press CTRL+C to stop server` );
   } );
+
+
 })();
