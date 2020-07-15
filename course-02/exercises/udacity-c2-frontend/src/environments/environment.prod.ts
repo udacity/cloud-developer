3bc26e42687c712fa05fa9f2e66fac311500575a
Randal Parsons <Randal.Parsons@lucidogroup.io>
2020-07-15 19:25:43 +1000

    Add AWS endpoint

--- course-02/exercises/udacity-c2-frontend/src/environments/environment.prod.ts
+++ course-02/exercises/udacity-c2-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagram-randal-dev-dev.ap-southeast-2.elasticbeanstalk.com/api/v0'
 };
 
 /*
