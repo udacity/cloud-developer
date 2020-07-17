bcf4c578aaadb4cdb6ca33287b7d465e368f5dfd
Marko Draksic <marko.draksic@vegvesen.no>
2020-07-17 15:41:08 +0200

    Rest api server, deployment config

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,23 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+describe('concat', () => {
+
+  it('should concat udacity and nanodegree', () => {
+    const result = concat('udacity','nanodegree');
+    expect(result).to.equal('udacity nanodegree');
+  });
+
+  it('should concate cloud and developer', () => {
+    const result = concat('cloud','developer');
+    expect(result).to.equal('cloud developer');
+  });
+
+  it('should throw an error if first string is empty', () => {
+    expect(()=>{ concat('','developer') }).to.throw('empty string not allowed')
+  });
+
+  it('should throw an error if second string is empty', () => {
+    expect(()=>{ concat('cloud','') }).to.throw('empty string not allowed')
+  });
+});
