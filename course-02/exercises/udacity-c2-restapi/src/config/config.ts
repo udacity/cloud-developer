469b64a027c16fc6da338022010cd3b4d3d810f7
cloud-soni <63272889+cloud-soni@users.noreply.github.com>
2020-07-16 19:57:19 -0600

    Update config.ts

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,21 +1,4 @@
 export const config = {
-  "dev": {
-    "username": "udagram_soni_dev",
-    "password": "L!5.4.run.284",
-    "database": "udagram-soni-dev",
-    "host": "udagram-soni-dev.cjytjdchnxuc.us-east-2.rds.amazonaws.com",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-soni-dev-bucket"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  },
   "postgress": {
     "username": process.env.POSTGRESS_USERNAME,
     "password": process.env.POSTGRESS_PASSWORD,
