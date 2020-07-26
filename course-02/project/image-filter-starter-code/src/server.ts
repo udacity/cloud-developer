4e7e83bee08b0ad6bf40f351fb950abecc7cc76c
Kesar Pandita <kesar@simsol.in>
2020-07-26 11:20:59 +0530

    created endpoint to filter image provided from the url and some validation added and created function to delete file from local system

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {filterImageFromURL, deleteLocalFiles, isUrl, isUrlAnImage} from './util/util';
+import fs from 'fs';
+import path from 'path';
 
 (async () => {
 
@@ -13,28 +15,57 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get('/filteredimage/', async(req, res) =>{
 
-  /**************************************************************************** */
+      let {image_url} = req.query
+      let isUrlImage, validUrl 
 
-  //! END @TODO1
+      // will throw error if url is not provided or image_url is provided as query field
+      if(!image_url){
+        res.status(404).send({"error": "Inavlid query field or Image Url not provided"})
+      }
+      else{
+        
+        // will check if the url provided is valid or not and will throw error if inValid 
+        validUrl = await isUrl(image_url)
+
+        if(!validUrl){
+          res.status(400).send({"error": "Invalid Url Provided"})
+        }
+        
+        // will check if the url provided is of an image with extension jpeg, jpg, gif and png
+        isUrlImage = await isUrlAnImage(image_url)
+
+        if(!isUrlImage){
+          res.status(404).send({"error": "Invalid Image Url provided"})
+        }
+      }
+
+      // will allow filtering of image if query field and the url provided is valid and the url is of an image
+      if(image_url && isUrlImage && validUrl){
+        let filteredpath = await filterImageFromURL(image_url)
+        res.status(201).sendFile(filteredpath)
+      }
+
+      const directoryPath = path.join(__dirname, '/util/tmp')
+      
+      fs.readdir(directoryPath, function(err, files) {
+        if (err) {
+
+          res.status(400).send({"error messag": err})
+
+        } else {
+      
+          deleteLocalFiles(files, directoryPath)
+        
+        }
+      })
+  })
   
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+    res.send("Please provide image Url")
   } );
   
 
