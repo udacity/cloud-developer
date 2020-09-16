5e0e8a90d68b272d6450640f31ea4c37098e2204
GerBoyle <ger_boyle@yahoo.ie>
2020-09-16 01:43:07 +0100

    frontend commit

--- course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
+++ course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   apiHost: 'http://localhost:8080/api/v0'
+  //apiHost: 'http://udagram-boyle-dev-dev.eu-west-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
