e0b6e494b58fd1435ea1350ef99542409805ce02
RTAndrew <anderson_rodax@hotmail.com>
2020-09-19 19:44:42 +0100

    refactor

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -11,15 +11,21 @@ import * as EmailValidator from 'email-validator';
 const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-    //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+  //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+  return;
 }
 
-async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
-    //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
+async function comparePasswords(
+  plainTextPassword: string,
+  hash: string
+): Promise<boolean> {
+  //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
+  return;
 }
 
 function generateJWT(user: User): string {
     //@TODO Use jwt to create a new JWT Payload containing
+    return ;
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
@@ -27,13 +33,13 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     // if (!req.headers || !req.headers.authorization){
     //     return res.status(401).send({ message: 'No authorization headers.' });
     // }
-    
+
 
     // const token_bearer = req.headers.authorization.split(' ');
     // if(token_bearer.length != 2){
     //     return res.status(401).send({ message: 'Malformed token.' });
     // }
-    
+
     // const token = token_bearer[1];
 
     // return jwt.verify(token, "hello", (err, decoded) => {
@@ -44,8 +50,8 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     // });
 }
 
-router.get('/verification', 
-    requireAuth, 
+router.get('/verification',
+    requireAuth,
     async (req: Request, res: Response) => {
         return res.status(200).send({ auth: true, message: 'Authenticated.' });
 });
