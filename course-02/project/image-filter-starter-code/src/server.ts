54d09c683980ee739bf4da57333b251ece726b66
GerBoyle <ger_boyle@yahoo.ie>
2020-09-19 22:24:27 +0100

    Test commit

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -2,10 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
-
-
-
-
 (async () => {
 
   // Init the Express application
