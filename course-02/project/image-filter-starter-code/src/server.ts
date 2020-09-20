3a3b7a4d8beb8631cb940b20219e4efd6f622bf1
Marko Draksic <marko.draksic@vegvesen.no>
2020-09-20 19:28:32 +0200

    Filter image implementation

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -30,17 +30,33 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get("/filteredimage/", async (req, res, next) => {
+    let { image_url } = req.query;
+
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`image url is required`);
+    }
+
+    const filteredpath = await filterImageFromURL(image_url);
+    return res.status(200).sendFile(filteredpath, {}, (err) => {
+      if (err) {
+        next(err);
+      } 
+      deleteLocalFiles([filteredpath]);
+    });
+  });
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
+  });
   
 
   // Start the Server
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
       console.log( `press CTRL+C to stop server` );
-  } );
+  });
 })();
