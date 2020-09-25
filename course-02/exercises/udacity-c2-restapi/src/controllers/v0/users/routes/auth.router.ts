c153969d5c95ed431312aacbbb14d308a7ad01a0
RTAndrew <anderson_rodax@hotmail.com>
2020-09-25 18:36:31 +0100

    feature: implemet bcrypt for handling passwords

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -9,18 +9,18 @@ import { NextFunction } from 'connect';
 import * as EmailValidator from 'email-validator';
 
 const router: Router = Router();
+let saltRounds = 10;
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-  //@TODO Use Bcrypt to Generated Salted Hashed Passwords
-  return;
+  const salt = await bcrypt.genSalt(saltRounds);
+  return await bcrypt.hash(plainTextPassword, salt);
 }
 
 async function comparePasswords(
   plainTextPassword: string,
   hash: string
 ): Promise<boolean> {
-  //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-  return;
+  return await bcrypt.compare(plainTextPassword, hash);
 }
 
 function generateJWT(user: User): string {
