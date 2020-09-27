e8c3b7c08a131cb89d9b15cb276f79152cfa85d8
Niall Maher <niall@codu.ie>
2020-09-27 20:35:04 +0100

    Lesson 5 start point - auth start

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -1,18 +1,19 @@
-import AWS = require('aws-sdk');
-import { config } from './config/config';
+import AWS = require("aws-sdk");
+import { config } from "./config/config";
 
 const c = config.dev;
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.credentials = credentials;
+if (c.aws_profile !== "DEPLOYED") {
+  var credentials = new AWS.SharedIniFileCredentials({ profile: "default" });
+  AWS.config.credentials = credentials;
+}
 
 export const s3 = new AWS.S3({
-  signatureVersion: 'v4',
+  signatureVersion: "v4",
   region: c.aws_region,
-  params: {Bucket: c.aws_media_bucket}
+  params: { Bucket: c.aws_media_bucket },
 });
-
 
 /* getGetSignedUrl generates an aws signed url to retreive an item
  * @Params
@@ -20,17 +21,16 @@ export const s3 = new AWS.S3({
  * @Returns:
  *    a url as a string
  */
-export function getGetSignedUrl( key: string ): string{
+export function getGetSignedUrl(key: string): string {
+  const signedUrlExpireSeconds = 60 * 5;
 
-  const signedUrlExpireSeconds = 60 * 5
+  const url = s3.getSignedUrl("getObject", {
+    Bucket: c.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
 
-    const url = s3.getSignedUrl('getObject', {
-        Bucket: c.aws_media_bucket,
-        Key: key,
-        Expires: signedUrlExpireSeconds
-      });
-
-    return url;
+  return url;
 }
 
 /* getPutSignedUrl generates an aws signed url to put an item
@@ -39,15 +39,14 @@ export function getGetSignedUrl( key: string ): string{
  * @Returns:
  *    a url as a string
  */
-export function getPutSignedUrl( key: string ){
+export function getPutSignedUrl(key: string) {
+  const signedUrlExpireSeconds = 60 * 5;
 
-    const signedUrlExpireSeconds = 60 * 5
+  const url = s3.getSignedUrl("putObject", {
+    Bucket: c.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
 
-    const url = s3.getSignedUrl('putObject', {
-      Bucket: c.aws_media_bucket,
-      Key: key,
-      Expires: signedUrlExpireSeconds
-    });
-
-    return url;
+  return url;
 }
--- course-02/exercises/udacity-c2-restapi/src/sequelize.ts
+++ course-02/exercises/udacity-c2-restapi/src/sequelize.ts
@@ -1,17 +1,15 @@
-import {Sequelize} from 'sequelize-typescript';
-import { config } from './config/config';
-
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
 
 const c = config.dev;
 
 // Instantiate new Sequelize instance!
 export const sequelize = new Sequelize({
-  "username": c.username,
-  "password": c.password,
-  "database": c.database,
-  "host":     c.host,
+  username: c.username,
+  password: c.password,
+  database: c.database,
+  host: c.host,
 
-  dialect: 'postgres',
-  storage: ':memory:',
+  dialect: "postgres",
+  storage: ":memory:",
 });
-
