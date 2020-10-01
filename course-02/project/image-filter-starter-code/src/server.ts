c03524ecfd142bf79f4f01a7ec1429b00395650a
Steve Meddaugh <steve.meddaugh@symetra.com>
2020-09-30 20:29:38 -0700

    Completed coding for project 2

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,23 +13,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
   /**************************************************************************** */
+  app.get("/filteredimage/", async (req, res) => {
+    const image_url = req.query.image_url;
 
-  //! END @TODO1
+    //validate the image_url
+    if (!image_url) {
+      return res.status(400).send("Image url is missing or not valid.");
+    }
+
+    try {
+      //filter the image
+      const filteredImage = await filterImageFromURL(image_url);
+
+      //send the filtered image then delete it
+      return res.status(200).sendFile(filteredImage, () => deleteLocalFiles([filteredImage]));
+    } catch (err) {
+      return res.status(422).send("Unable to load photo.");
+    }
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,3 +1,4 @@
+import { reject } from 'bluebird';
 import fs from 'fs';
 import Jimp = require('jimp');
 
@@ -9,8 +10,13 @@ import Jimp = require('jimp');
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
+    return new Promise( async (resolve, reject) => {
+        let photo
+        try {
+            photo = await Jimp.read(inputURL);
+        } catch (err) {
+            return reject(err);
+        }
         const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
         await photo
         .resize(256, 256) // resize
