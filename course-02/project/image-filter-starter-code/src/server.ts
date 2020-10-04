19c97afb3248db8d2d2a0bfc4aeba1be974c566c
Niall Maher <niall@codu.ie>
2020-10-04 22:05:00 +0100

    Adds functionality and error handling for tasks

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,18 +1,41 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import fs from "fs";
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+  app.get("/filteredimage", async (req, res) => {
+    const { image_url } = req.query;
+
+    if (!image_url) {
+      return res.status(400).send({ message: "image_url is required" });
+    }
+
+    try {
+      const filteredUrl = await filterImageFromURL(image_url);
+      res.status(200).sendFile(filteredUrl, {}, (err) => {
+        if (err) {
+          res.status(500).send({ message: "Something went wrong" });
+        } else {
+          res.status(200);
+        }
+        deleteLocalFiles([filteredUrl]).catch((err) => {
+          console.log({ err });
+        });
+      });
+    } catch {
+      res.status(422).send({ message: "Unsupported request" });
+    }
+  });
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -30,17 +53,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
