230954ce3e270399a5122bd665fa118a45488543
Ashbyt <abythell@Hotmail.com>
2020-10-11 21:36:14 +1100

    Add files via upload

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,7 +10,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -29,18 +30,37 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get("/filteredimage/", async (req: Request, res: Response) => {
+    let { image_url } = req.query;
+
+    if (!image_url) {
+      return res.status(400).send(`image_url is required`);
+    }
+
+    try {
+      const filteredpath = await filterImageFromURL(image_url)
+
+      await res.status(200).sendFile(filteredpath, {}, (err) => {
+        if (err) { return res.status(422).send(`unable to process the image`); }
+        deleteLocalFiles([filteredpath])
+      })
+    } catch (err) {
+      res.status(422).send(`unable to process the image`);
+    }
+  });
+
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.status(200).send("try GET /filteredimage?image_url={{}}")
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
