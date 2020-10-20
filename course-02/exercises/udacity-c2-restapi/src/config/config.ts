122d5eed279cd4e2717124161c2f8f52a42f6905
Ayahhamdi <Ayahhamdi@gmail.com>
2020-10-20 11:19:38 +0400

    put method is successfully added

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -9,6 +9,9 @@ export const config = {
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
+  "jwt" : {
+    "secret" : "helloworld"
+  },
   "prod": {
     "username": "",
     "password": "",
