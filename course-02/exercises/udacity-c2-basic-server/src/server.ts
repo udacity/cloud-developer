41fd3b15a03a908724f0fa95ff78d8bd91a994fd
Dave Roye <daveroye@gmail.com>
2020-10-31 08:46:53 -0500

    Update server.ts

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -70,13 +70,80 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get("/cars/", (req: Request, res: Response) => {
+    let { make } = req.query;
+
+    if (!make) {
+      return res.status(200)
+        .send(cars);
+    }
+
+    let fcars = cars;
+    fcars = cars.filter((car) => car.make === make);
+
+    if (fcars.length == 0) {
+      return res.status(400)
+        .send('no cars found matching that make')
+    }
+
+    return res.status(200)
+      .send(fcars);
+  });
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get( "/cars/:id", ( req: Request, res: Response ) => {
+    let { id } = req.query;
+
+    if ( !id ) {
+      return res.status(400)
+                .send(`car ID is required`);
+    }
+
+    const car = cars.filter((car) => car.id == id);
+
+    if ( car && car.length == 0 ) {
+      return res.status(400)
+                .send('no car found matching that ID')
+    }
+
+    return res.status(200)
+              .send(car);
+  } );
 
   /// @TODO Add an endpoint to post a new car to our list
-  // it should require id, type, model, and cost
+  // it should require make, type, model, and cost
+  app.post("/cars",
+    async (req: Request, res: Response) => {
+
+      const car: Car = req.body;
+
+      if (!car) {
+        return res.status(400)
+          .send(`car info required`);
+      }
+
+      if (!car.make) {
+        return res.status(400)
+          .send('no car make found in car info');
+      } else if (!car.model) {
+        return res.status(400)
+          .send('no car model found in car info');
+      } else if (!car.cost) {
+        return res.status(400)
+          .send('no car cost found in car info');
+      } else if (!car.type) {
+        return res.status(400)
+          .send('no car type found in car info');
+      }
+
+      car.id = cars.length;
+      cars.push(car);
+      return res.status(200)
+        .send('new car added');
+    }
+  );
 
   // Start the Server
   app.listen( port, () => {
