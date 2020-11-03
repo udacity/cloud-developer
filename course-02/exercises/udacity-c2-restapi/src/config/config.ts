f4ce773653b4cd6286dd2c90c2fc685dcbe1f633
Dave Roye <daveroye@gmail.com>
2020-11-03 10:32:54 -0600

    added auth changes

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -18,5 +18,8 @@ export const config = {
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
   }
 }
