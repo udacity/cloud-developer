5bf8092be622526479b75bbe9eb6ac7f0c576f42
RTAndrew <anderson_rodax@hotmail.com>
2020-11-14 17:54:18 +0100

    fix: fix cors issue when there isnt no todos

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -1,11 +1,42 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+import { getTodoById } from '../../businessLogic/todos'
+import { getPresignedUrl } from '../../businessLogic/attachmentUrl'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log(event)
-  // const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
-}
+const logger = createLogger('generateUploadUrl')
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
+
+  const result = await getTodoById(userId, todoId)
+
+  if (result.Count === 0) {
+    logger.warn(`user ${userId} requesting Presigned URL for non exists record: ${todoId}`)
+
+    return {
+      statusCode: 400,
+      body: JSON.stringify(`Todo does not exist.`)
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      uploadUrl: getPresignedUrl(todoId)
+    })
+  }
+})
+
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -5,15 +5,26 @@ import { cors } from 'middy/middlewares'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { getUserId } from '../utils'
 import { getTodos } from '../../businessLogic/todos'
-import { createLogger } from '../../utils/logger'
-
-const logger = createLogger('getTodos')
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
   const user_id = getUserId(event)
-  logger.info(`Retrieving items for user ${user_id}`)
+
   const todoItems = await getTodos(user_id)
+
+  if (todoItems.length === 0) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        items: []
+      })
+    }
+  }
+
   return {
     statusCode: 200,
     headers: {
@@ -24,7 +35,6 @@ export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGat
       items: todoItems
     })
   }
-
 })
 
 
