236792c9df30bf68f875d80f796b8a439bde783c
RTAndrew <anderson_rodax@hotmail.com>
2020-11-14 23:22:36 +0100

    feature(clientRepo): update config file

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'gf1u4xxi1m'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev--zsd3hiu.us.auth0.com',            // Auth0 domain
+  clientId: '8zNSPfXVtbvXsEpmMdOiD3gurmqqWVca',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
