6f6b3a7111426c9b501e8f9732a848c20fa4bb0b
RTAndrew <anderson_rodax@hotmail.com>
2020-11-14 13:55:10 +0100

    feature: get todos

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,37 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { getUserId } from '../utils'
+import { getTodos } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
-}
+const logger = createLogger('getTodos')
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+  const user_id = getUserId(event)
+  logger.info(`Retrieving items for user ${user_id}`)
+  const todoItems = await getTodos(user_id)
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      items: todoItems
+    })
+  }
+
+})
+
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
+
+
