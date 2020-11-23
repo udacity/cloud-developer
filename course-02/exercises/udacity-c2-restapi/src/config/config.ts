d9268660f2a4a7422b80f6d0cd19a4735c3d84cb
ajaykrishna2013 <ajaykrishna@gmail.com>
2020-11-22 22:33:16 -0800

    implement jwt

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -11,6 +11,9 @@ export const config = {
       "aws_region": process.env.AWS_REGION,
       "aws_profile": process.env.AWS_PROFILE,
       "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+    },
+    "jwt": {
+      "secret" : process.env.JWT_SECRET
     }
   },
   "prod": {
