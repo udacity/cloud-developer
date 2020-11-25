ddf4e2354872094c84be3875f421ed059bfe92ff
ajaykrishna2013 <ajaykrishna@gmail.com>
2020-11-24 21:01:49 -0800

    pass user as an object to generate JWT

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -26,7 +26,7 @@ async function comparePasswords(plainTextPassword: string, hash: string): Promis
 
 function generateJWT(user: User): string {
     //@TODO Use jwt to create a new JWT Payload containing
-    return jwt.sign(user, config.dev.jwt.secret);
+    return jwt.sign({user}, config.dev.jwt.secret);
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
