32e5b4aabc5ba75744ebdd94e8c23799c6683e7b
EmmanuelManana <emanana@student.wethinkcode.co.za>
2020-11-29 20:41:17 +0200

    image filter complete

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filter } from 'bluebird';
 
 (async () => {
 
@@ -17,24 +18,43 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
+  app.get('/filteredimage', async (req: express.Request, res: express.Response) => {
+    //    1. validate the image_url query
+    const { image_url: imageUrl } = req.query;
+    if (!imageUrl) {
+      res.statusCode = 400;
+      return res.send("Bad Request");
+    }
+    try {
+       //    2. call filterImageFromURL(image_url) to filter the image
+      const filtered = await filterImageFromURL(imageUrl);
+      res.on('finish', () => {
+        try {
+          //    4. deletes any files on the server on finish of the response
+          deleteLocalFiles([filtered]);
+        } catch {
+          console.error(`Error deleting ${filtered}`);
+        }
+      });
+     // 3. send the resulting file in the response
+      return res.sendFile(filtered);
+    } catch (e) {
+      res.statusCode = 500;
+      return res.send(e.message || 'Unknown server error');
+    }
+  });
 
   //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+  app.get( '/', async ( req, res ) => {
+    res.send('try GET /filteredimage?image_url={{}}')
   } );
   
 
