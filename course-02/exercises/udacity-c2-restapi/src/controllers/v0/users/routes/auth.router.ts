15d1dbb79ce8cfe5420ae7b7977e88d32ea6c798
Zilvinas Marozas <zilvinas_m@hotmail.com>
2020-12-12 18:05:11 -0500

    fixed issues

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -12,14 +12,17 @@ const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
     //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+    return "";
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
+    return true;
 }
 
 function generateJWT(user: User): string {
     //@TODO Use jwt to create a new JWT Payload containing
+    return "";
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
