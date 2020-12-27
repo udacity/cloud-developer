2211c7c63c78d725dd33ccdcbd9c422ef59d87b2
Jeanette Fernandez <jeanette.fernandez@virginpulse.com>
2020-12-27 18:09:33 -0500

    Lesson-5-service-registration: copied from the previous lesson to continue to work from my progress.

--- course-03-nd9990-c3-microservices-v1/lesson-5-Service-Registration/exercises/udacity-c3-restapi-feed/src/config/config.ts
+++ course-03-nd9990-c3-microservices-v1/lesson-5-Service-Registration/exercises/udacity-c3-restapi-feed/src/config/config.ts
@@ -1,24 +1,14 @@
-export const config = {
-  "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
-    "host": process.env.POSTGRESS_HOST,
-    "dialect": "postgres",
-    "aws_region": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET,
-    "url": process.env.URL    
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  },
-  "jwt": {
-    "secret": process.env.JWT_SECRET
-  }
 
-}
+export const config = {
+  'dev': {
+    'username': process.env.POSTGRES_USERNAME,
+    'password': process.env.POSTGRES_PASSWORD,
+    'database': process.env.POSTGRES_DATABASE,
+    'host': process.env.POSTGRES_HOST,
+    'dialect': 'postgres',
+    'aws_region': process.env.AWS_REGION,
+    'aws_profile': process.env.AWS_PROFILE,
+    'aws_media_bucket': process.env.AWS_MEDIA_BUCKET,
+    'jwt_secret': process.env.JWT_SECRET
+  }
+};
