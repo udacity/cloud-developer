3b9e2266695edd6ea7e2f3daa41cb60811a7b482
aalzate <aalzate@sonatype.com>
2020-12-27 01:13:38 -0500

    Update project configuration

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,14 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  "aws": {
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
+  "dev": {
+    "username": process.env.POSTGREESS_USERNAME,
+    "password": process.env.POSTGREESS_PASSWORD,
+    "database": process.env.POSTGREESS_DATABASE,
+    "host": process.env.POSTGREESS_HOST,
     "dialect": "postgres"
   }
 }
