de7e975bc88d7e3d0d979ef6f5b236f28ab5d2d4
Jamie Webb <webbjamie93@gmail.com>
2021-01-10 17:42:53 +0000

    project 2 submission

--- course-02/project/image-filter-starter-code/README.md
+++ course-02/project/image-filter-starter-code/README.md
@@ -1,3 +1,6 @@
+Endpoint:
+http://image-filter-starter-code-dev222222222222222222.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://d20vrrgs8k4bvw.cloudfront.net/images/open-graph/udacity.png
+
 # Udagram Image Filtering Microservice
 
 Udagram is a simple cloud application developed alongside the Udacity Cloud Engineering Nanodegree. It allows users to register and log into a web client, post photos to the feed, and process photos using an image filtering microservice.
--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,23 +13,20 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
+  app.get( "/filteredimage", async ( req, res ) => {
+    if (!req.query.image_url) {
+      res.status(400).send("Please provide an image_url query parameter");
+    }
+    try {
+      const image = await filterImageFromURL(req.query.image_url);
+      res.sendFile(image);
+      res.on('finish', function() {
+        deleteLocalFiles([image]);
+      });
+    } catch {
+      res.status(500).send("Unable to process provided image");
+    }
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
