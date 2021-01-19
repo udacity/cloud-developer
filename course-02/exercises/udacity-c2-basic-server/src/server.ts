e74a82cf97d5615927d2ec4a5a70016914db496d
sandeep kesarwani <sandeep.kesarwani@mechademy.com>
2021-01-19 10:58:26 +0530

    UPDATE: new enpoints added

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -70,14 +70,55 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get("/cars",
+    async(  req: Request, res: Response) =>  {
+      const { make } = req.query;
+      if(!make) {
+        return res.status(200).send(cars);
+      }
+      else  {
+        return res.status(200).send(cars.map((item)=>{
+          if(item.make === make) return item;
+        }));
+      }
+    }
+  );
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get("/cars/:id",
+  async(  req: Request, res: Response) =>  {
+    const { id } = req.params;
+    let myCar: Object;
+    cars.forEach((item)=>{
+      if(item.id == id)  {
+        myCar = item;
+      }
+    });   
+    if(myCar)  
+     return res.status(200).send(myCar);
+    else
+      return res.status(400).send("Not found");
+  }
+);
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
 
+  app.post("/car",
+  async(  req: Request, res: Response) =>  {
+    const { make,type, model, cost } = req.body;
+    cars.push({
+      make: make,
+      type: type,
+      model: model,
+      cost: cost,
+      id: cars.length
+    });
+    return res.status(200).send(cars);
+  }
+);
   // Start the Server
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
