bf649ebc67470efda89b1382ce0a2bd4082aad10
Jeferson Santos <jefo2k@gmail.com>
2021-01-31 18:40:29 -0300

    add get a list of cars, get a specific car and post a new car endpoints

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -70,13 +70,66 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get( "/cars/", 
+    ( req: Request, res: Response ) => {
+      const { make } = req.query;
+
+      let cars = cars_list;
+
+      if (make) {
+        cars = cars_list.filter(e => e.make === make)
+      }
+
+      res.status(200).send(cars);
+  } );
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get( "/cars/:id",
+    (req: Request, res: Response) => {
+
+      let { id } = req.params;
+
+      const car = cars_list.find(e => e.id === parseInt(id))
+
+      if ( !car ) {
+        return res.status(404)
+                  .send(`car not found!`);
+      }
+
+      return res.status(200).send(car);
+    } );
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+  app.post( "/cars", 
+    (req: Request, res: Response) => {
+      const { id, make, type, model, cost } = req.body;
+
+      if ( !id || !make || !type || !model || !cost ) {
+        console.log(id, make, type, model, cost);
+        console.log(!id, !make, !type, !model, !cost);
+
+        return res.status(400)
+                  .send({ error: 'incomplete payload.'});
+      }
+
+      const newCar: Car = {
+        id,
+        make,
+        type,
+        model,
+        cost
+      };
+
+      cars_list.push(newCar);
+
+      // send the complete car object as a response
+      // along with 201 - creation success
+      return res.status(201).send(newCar);
+
+    } );
 
   // Start the Server
   app.listen( port, () => {
