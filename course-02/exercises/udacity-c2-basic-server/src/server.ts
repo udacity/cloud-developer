1dff765aa1a5c4976d7902126a0cf816c55eabff
Ricardo Ardiles <ricardoardiles@MacBook-Pro-de-Ricardo.local>
2021-03-22 23:00:29 -0300

    add: small change to test pull request

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -112,7 +112,7 @@ import { Car, cars as cars_list } from './cars';
 
     if (!id || !type || !model || !cost){
       return res.status(400)
-                .send('make, type, model, cost, id are required');
+                .send('make, type, model, cost, id are required now');
     }
 
     const new_car: Car = {
