cf4683b53a97a8cc200030600f84081759ef4eb1
Kazeem Ganiu <ganiukazeem@gmail.com>
2021-03-23 11:07:05 +0100

    first commit

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "postgres",
+    "password": "postgres",
+    "database": "postgres",
+    "host": "database-3.cd8aqdzpznqo.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -17,7 +17,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -26,6 +25,33 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  app.get("/filteredimage",async (req,res)=>{
+    // console.log("in");
+    // console.log(req.query)
+    let image_url  = req.query.image_url;
+    
+    // res.send(image_url)
+
+    //Validate url
+    const isValideUrl = image_url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    // console.log("is valid", isValideUrl)
+   
+    if(isValideUrl == null)
+      return res.status(400).send(`Inavlid url! Try again with valid url`);
+    else{
+    //Process Image
+      const filteredImage = filterImageFromURL(image_url);
+      if(filteredImage===undefined||filteredImage===null)
+        return res.status(400).send(`Unable to filter image`);
+      else
+        filteredImage.then(function(output){
+          // console.log(output)
+          return res.status(200).sendFile(output);
+        }).catch(error => console.log("erororo"))
+        // return res.status(200).sendFile(filteredImage);
+    }
+  })
 
   /**************************************************************************** */
 
@@ -36,6 +62,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
+  
   
 
   // Start the Server
