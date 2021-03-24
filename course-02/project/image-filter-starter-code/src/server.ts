35ad414087e79fb05da97e04a7b763c05dfaff43
Ricardo Ardiles <ricardo.ardiles@pagofacil.cl>
2021-03-24 16:07:42 -0300

    refactoring

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,7 +1,11 @@
+require('dotenv').config();
 import express from 'express';
 import { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {filterImageFromURL, deleteLocalFiles} from './controller/v0/util/util';
+
+import { IndexRouter } from './controller/v0/index.router';
+
 import { nextTick } from 'process';
 
 const fs = require('fs');
@@ -17,50 +21,7 @@ const fs = require('fs');
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-  app.get('/filteredimage/', async (req: Request, res: Response) => {
-    let {image_url } = req.query;
-
-    if (!image_url) {
-      return res.status(400).send('image_url is required');
-    }
-    const new_image = await filterImageFromURL(image_url);
-
-    // after finish the response, the image is deleted.
-    res.on('finish', function() {
-      try{
-        const pathTemp = __dirname + '/util/tmp';
-        const filesToDelete = [];
-        const files = fs.readdirSync(pathTemp);
-        for (const file of files) {
-          filesToDelete.push(pathTemp + '/' + file)
-        }
-        deleteLocalFiles(filesToDelete);
-      }
-      catch(error){
-        console.log(error);
-      }
-      
-      });
-
-    return res.sendFile(new_image);
-});
-
-  //! END @TODO1
+  app.use('/', IndexRouter)
   
   // Root Endpoint
   // Displays a simple message to the user
