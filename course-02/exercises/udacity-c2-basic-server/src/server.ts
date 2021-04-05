e8d0f0193f4db3d9c5ce5a5fbeeaac5fc380576a
Angelika-Kachur <angelika.kachur@gmail.com>
2021-04-06 00:14:14 +0300

    Add cars

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -71,6 +71,21 @@ import { Car, cars as cars_list } from './cars';
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
 
+  // Get a greeting to a specific person to demonstrate req.query
+  // > try it {{host}}/persons?name=the_name
+  app.get( "/cars/", ( req: Request, res: Response ) => {
+    let { make } = req.query;
+
+    if ( make ) {
+      return res.status(400)
+                .send(`name is required`);
+    }
+
+    return res.status(200)
+              .send(`Welcome to the Cloud, ${name}!`);
+
+  } );
+
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
