bfbc2385f3decd490fd76fa6ecfb92ffa8c0bbbf
soinegve <karolos.kantounis@gmail.com>
2021-04-23 10:10:10 -0400

    adding changes requested

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -13,23 +13,27 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
 
-  //! END @TODO1
+  app.get( "/filteredimage/",  async ( req: Request, res: Response ) => {
+    let { image_url } = req.query;
+
+    validate_image_url(image_url, res);
+
+    
+  
+
+     filterImageFromURL(image_url).
+      then ( (v)=> { 
+        res.status(200).sendFile(v)
+        delete_temp_file(res, v);
+      }
+      )
+      .catch( (x)=>{ res.status(400).send("something went wrong while fetching " + image_url);})
+
+     
+    
+  } );
   
   // Root Endpoint
   // Displays a simple message to the user
@@ -44,3 +48,14 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
       console.log( `press CTRL+C to stop server` );
   } );
 })();
+
+function delete_temp_file(res: express.Response, v: string) {
+  res.on('finish', () => { deleteLocalFiles([v]); });
+}
+
+function validate_image_url(image_url: any, res: express.Response) {
+  if (!image_url) {
+    res.status(400)
+      .send(`image_url is required`);
+  }
+}
