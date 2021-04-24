30aa0bafd36083b0e43607e90cbab05bead77566
shannonguyot <shannonguyot97@gmail.com>
2021-04-24 13:41:55 -0400

    updating files for deployment

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -14,23 +14,7 @@ import { resolve } from 'path';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
+  // filter an image
   app.get( "/filteredimage/", async ( req, res ) => {
     const { image_url } = req.query;
 
@@ -44,8 +28,14 @@ import { resolve } from 'path';
       return res.status(400).send(`unable to filter image for url of ${image_url}`);
     }
 
-    const resPromise = new Promise(() => { res.status(200).sendFile(filterImage) });
-    resPromise.then(() => { deleteLocalFiles([filterImage]) });
+    res.status(200).sendFile(filterImage, (err) => {
+      if ( err ) {
+        console.log(`error occurred in file transfer`);
+      }
+      else {
+        deleteLocalFiles([filterImage]);
+      }
+    });
   } );
   
   // Root Endpoint
