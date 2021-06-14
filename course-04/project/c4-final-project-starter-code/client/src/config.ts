55f7cd6910194506d62d1ac82a0dcbb83d79e88a
nanotower <fernandodelatorre4@gmail.com>
2021-06-14 18:24:19 +0200

    Add front config

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '012u3148wl'
+const apiId = 'fo6qub5e53'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 // export const apiEndpoint = 'http://localhost:3003/dev'
 
