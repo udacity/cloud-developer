e10a0b53d951b7cad768ad6b63d81aec3a85d628
Julia Hebauf <julia.hebauf@lyth.de>
2021-07-01 14:19:59 +0200

    P2L4: Building and Deploying

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -1,53 +1,51 @@
 import AWS = require('aws-sdk');
 import { config } from './config/config';
 
-const c = config.dev;
+const c = config.aws;
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+var credentials = new AWS.SharedIniFileCredentials({ profile: c.aws_profile });
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
   region: c.aws_region,
-  params: {Bucket: c.aws_media_bucket}
+  params: { Bucket: c.aws_media_bucket }
 });
 
 
-/* getGetSignedUrl generates an aws signed url to retreive an item
- * @Params
- *    key: string - the filename to be put into the s3 bucket
- * @Returns:
- *    a url as a string
+/**
+ * getGetSignedUrl generates an aws signed url to retreive an item
+ * @param key  string, the filename to be put into the s3 bucket
+ * @returns  a url as a string
  */
-export function getGetSignedUrl( key: string ): string{
+export function getGetSignedUrl(key: string): string {
 
-  const signedUrlExpireSeconds = 60 * 5
+  const signedUrlExpireSeconds = 60 * 5;
 
-    const url = s3.getSignedUrl('getObject', {
-        Bucket: c.aws_media_bucket,
-        Key: key,
-        Expires: signedUrlExpireSeconds
-      });
+  const url = s3.getSignedUrl('getObject', {
+    Bucket: c.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds
+  });
 
-    return url;
+  return url;
 }
 
-/* getPutSignedUrl generates an aws signed url to put an item
- * @Params
- *    key: string - the filename to be retreived from s3 bucket
- * @Returns:
- *    a url as a string
+/**
+ * getPutSignedUrl generates an aws signed url to put an item
+ * @param key  string, the filename to be retreived from s3 bucket
+ * @returns  a url as a string
  */
-export function getPutSignedUrl( key: string ){
+export function getPutSignedUrl(key: string) {
 
-    const signedUrlExpireSeconds = 60 * 5
+  const signedUrlExpireSeconds = 60 * 5;
 
-    const url = s3.getSignedUrl('putObject', {
-      Bucket: c.aws_media_bucket,
-      Key: key,
-      Expires: signedUrlExpireSeconds
-    });
+  const url = s3.getSignedUrl('putObject', {
+    Bucket: c.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds
+  });
 
-    return url;
+  return url;
 }
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 import { FeedItem } from '../models/FeedItem';
 import { requireAuth } from '../../users/routes/auth.router';
 import * as AWS from '../../../../aws';
+import { isNumeric } from 'validator';
 
 const router: Router = Router();
 
@@ -16,15 +17,50 @@ router.get('/', async (req: Request, res: Response) => {
     res.send(items);
 });
 
-//@TODO
-//Add an endpoint to GET a specific resource by Primary Key
+//GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    let { id } = req.params;
+    if (!id || !isNumeric(id)) {
+        return res.status(400).send({ message: 'Required Id is not a number' });
+    }
+    const item = await FeedItem.findByPk(id);
+    if(item.url) {
+        item.url = AWS.getGetSignedUrl(item.url);
+    }
+    res.send(item);
+});
 
-// update a specific resource
-router.patch('/:id', 
-    requireAuth, 
-    async (req: Request, res: Response) => {
-        //@TODO try it yourself
-        res.send(500).send("not implemented")
+// Update a specific resource by Primary Key
+router.patch('/:id', requireAuth, async (req: Request, res: Response) => {
+    const caption = req.body.caption;
+    const fileName = req.body.url;
+    let { id } = req.params;
+ 
+    // check Caption is valid
+    if (!caption) {
+        return res.status(400).send({ message: 'Caption is required or malformed' });
+    }
+
+    // check Filename is valid
+    if (!fileName) {
+        return res.status(400).send({ message: 'File url is required' });
+    }
+
+    if (!id || !isNumeric(id)) {
+        return res.status(400).send({ message: 'Required Id is not a number' });
+    }
+
+    const item = await FeedItem.findByPk(id);
+    if (!item) {
+        return res.status(404).send({ message: `No feed with Id ${id} found` });
+    }
+    item.caption = caption;
+    item.url = fileName;
+
+    const saved_item = await item.save();
+    saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+
+    res.send(item);
 });
 
 
