7f6faaee866fe789445726f140f8d050f61a45a2
dilllip <dimart.spb@icloud.com>
2021-10-31 16:54:33 +0100

    WIP implement filtering API

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import validUrl from 'valid-url';
 
 (async () => {
 
@@ -13,23 +14,31 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     // Use the body parser middleware for post requests
     app.use(bodyParser.json());
 
-    // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
     // GET /filteredimage?image_url={{URL}}
     // endpoint to filter an image from a public url.
-    // IT SHOULD
-    //    1
-    //    1. validate the image_url query
-    //    2. call filterImageFromURL(image_url) to filter the image
-    //    3. send the resulting file in the response
-    //    4. deletes any files on the server on finish of the response
     // QUERY PARAMATERS
     //    image_url: URL of a publicly accessible image
     // RETURNS
-    //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+    //   the filtered image file
+    app.get("/filteredimage", async (req, res) => {
+        let { image_url } = req.query;
 
-    /**************************************************************************** */
+        // Validation
+        if (!validUrl.isWebUri(image_url)) {
+            res.status(400).send({ message: 'Image URL is required or malformed' });
+        }
 
-    //! END @TODO1
+        // The actual task
+        try {
+            let filteredImagePath = await filterImageFromURL(image_url);
+
+            res.sendFile(filteredImagePath, () => {
+                deleteLocalFiles([filteredImagePath]);
+            })
+        } catch (e) {
+            res.status(418).send({ message: "Processing failed" });
+        }
+    });
 
     // Root Endpoint
     // Displays a simple message to the user
--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -12,11 +12,12 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
     return new Promise(async resolve => {
         const photo = await Jimp.read(inputURL);
         const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+
         await photo
             .resize(256, 256) // resize
             .quality(60) // set JPEG quality
             .greyscale() // set greyscale
-            .write(__dirname + outpath, (img) => {
+            .write(__dirname + outpath, (_) => {
                 resolve(__dirname + outpath);
             });
     });
