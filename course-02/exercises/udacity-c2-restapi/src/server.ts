bc278e9212915cf77cf7998c7307308907ecc7a5
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-11-05 16:21:17 -0500

    tslint server.ts

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -13,23 +13,23 @@ import { V0MODELS } from './controllers/v0/model.index';
 
   const app = express();
   const port = process.env.PORT || 8080; // default port to listen
-  
+
   app.use(bodyParser.json());
 
-  //CORS Should be restricted
+  // CORS Should be restricted
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
   });
 
-  app.use('/api/v0/', IndexRouter)
+  app.use('/api/v0/', IndexRouter);
 
   // Root URI call
-  app.get( "/", async ( req, res ) => {
-    res.send( "/api/v0/" );
+  app.get( '/', async ( req, res ) => {
+    res.send( '/api/v0/' );
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
