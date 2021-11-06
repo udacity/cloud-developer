8945e9023b66bc444f3b21009950bf12128ffbf5
ruchi <ruchiverma6@gmail.com>
2021-11-06 20:43:41 +0530

    added changes

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://yjujr8vmr9.execute-api.us-east-1.amazonaws.com/dev'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
-  callbackUrl: 'http://localhost:3000/callback'
+  domain: 'dev-v32wyktu.us.auth0.com',
+  clientId: 'sTaAPQSlisA1r1IuOSm7xuMwK7PSocnH',
+  callbackUrl: 'http://localhost/3000/callback'
 }
