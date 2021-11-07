c7dc90dd7d82240cd193e0603afafcdb1f77ac14
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-11-07 12:42:54 -0600

    implements password hash and compare

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -12,12 +12,13 @@ const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
     // @TODO Use Bcrypt to Generated Salted Hashed Passwords
-    return 'NotYetImplemented';
+    const numberSaltRounds = 10;
+    return await bcrypt.hash(plainTextPassword, numberSaltRounds);
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
     // @TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-    return true;
+    return await bcrypt.compare(plainTextPassword, hash);
 }
 
 function generateJWT(user: User): string {
