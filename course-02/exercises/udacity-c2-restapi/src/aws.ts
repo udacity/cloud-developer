3b96d08918b6b1fa7afc6383cc8287af6fd20761
Tony Okusanya <Anthony.Okusanya@gmail.com>
2022-01-03 13:45:08 -0600

    updated authentication

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -4,8 +4,16 @@ import { config } from './config/config';
 const c = config.dev;
 
 //Configure AWS.
-var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
-AWS.config.credentials = credentials;
+//var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
+//AWS.config.credentials = credentials;
+
+
+
+//Configure AWS
+if(c.aws_profile !== "DEPLOYED") {
+  var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+  AWS.config.credentials = credentials;
+}
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
