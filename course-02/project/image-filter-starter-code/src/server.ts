ecbfb4ff134e65a22565d33945533ec25bbf9ae5
Chris McGrew <chris.mcgrew20@gmail.com>
2022-01-09 12:37:40 -0700

    chore: refactor

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -9,6 +9,9 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
+
+  // const BASIC_URL_REGEX: string = "https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]*\.[a-zA-Z0-9()]*\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)";
+  // const URL_REGEX: RegExp = new RegExp(BASIC_URL_REGEX);
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
@@ -18,9 +21,9 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // endpoint to filter an image from a public url.
   // IT SHOULD
   //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
+  //    1. validate the image_url query - DONE
+  //    2. call filterImageFromURL(image_url) to filter the image - DONE
+  //    3. send the resulting file in the response - done
   //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
@@ -33,8 +36,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+  app.get( "/filteredimage", async ( req, res,  ) => {
+  const {image_url}: {image_url: string} = req.query
+
+  if(!image_url) {
+    res.status(400).send("The required image_url was not provided");
+  }
+
+  // if(!image_url.match(URL_REGEX)) {
+  //   res.status(400).send("Malformed image_url");
+  // }
+
+  const path: string = await filterImageFromURL(image_url);
+  const patharray: Array<string> = new Array(path);
+
+  res.on('finish', () => {
+    deleteLocalFiles(patharray)
+  }).status(200).sendFile(path);
   } );
   
 
