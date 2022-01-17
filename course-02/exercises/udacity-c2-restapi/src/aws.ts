024529226d38e50a54f3aefa93ad3cb7bc8ae9ac
Sudhanshu Kulshrestha <48475411+SudKul@users.noreply.github.com>
2022-01-17 16:50:33 +0530

    Update aws.ts

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -4,9 +4,8 @@ import { config } from './config/config';
 const c = config.dev;
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
 if(c.aws_profile !== "DEPLOYED") {
-  var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+  var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
 }
 AWS.config.credentials = credentials;
 
