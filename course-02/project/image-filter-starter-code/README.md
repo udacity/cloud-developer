362b0c23652c97b75b837d016a408ef819c3db82
DangDn <doanngocdang96@gmail.com>
2022-04-24 21:45:06 +0700

    final-project

--- course-02/project/image-filter-starter-code/README.md
+++ course-02/project/image-filter-starter-code/README.md
@@ -46,3 +46,7 @@ Prevent requests without valid authentication headers.
 
 Add your own domain name and have it point to the running services (try adding a subdomain name to point to the processing server)
 > !NOTE: Domain names are not included in AWS’ free tier and will incur a cost.
+
+My Submission
+EB_URL: http://image-filter-starter-code-dev22.us-east-1.elasticbeanstalk.com/
+Github: https://github.com/dangdn721/cloud-developer/tree/master/course-02/project/image-filter-starter-code
--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,7 +28,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-
+  app.get( "/filteredimage", async (req: Request, res: Response) => {
+    const { image_url }  = req.query 
+    if (!image_url) {
+      return res.status(400).send('image_url is required')
+    }
+    try {
+      const filteredpath = await filterImageFromURL(image_url as string)
+      res.sendFile(filteredpath, (err) => {
+        deleteLocalFiles([filteredpath])
+        if (err) {
+          console.error(err)
+        }
+      })
+    }
+    catch (err) {
+      res.status(400).send('Something wrong the Image URL.')
+    }
+  });
   //! END @TODO1
   
   // Root Endpoint
