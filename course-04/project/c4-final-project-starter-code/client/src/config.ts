f2321e2fec20a5c19b1dd6231b5f867f26cc3ba9
Chris McGrew <chris.mcgrew20@gmail.com>
2022-04-30 13:53:30 -0700

    chore: fix auth

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,6 +1,7 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'axifpxt19j'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+// export const apiEndpoint = 'http://localhost:3002/dev'
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
