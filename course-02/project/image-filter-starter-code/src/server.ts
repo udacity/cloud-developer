e7822529337b73d6314af906eae5ef6f8432bd36
Samuel Ajayi <femiajayi705@gmail.com>
2022-05-02 01:21:55 +0200

    bugfix: Handled exception for images that can not be downloaded

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -14,21 +14,6 @@ import fs from "fs";
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query ✔️
-  //    2. call filterImageFromURL(image_url) to filter the image ✔️
-  //    3. send the resulting file in the response ✔️
-  //    4. deletes any files on the server on finish of the response ✔️
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
   app.get("/filteredimage", async (req, res, next) => {
     // get the image_url param from the reques
     const { image_url }: { image_url: string } = req.query
@@ -36,9 +21,14 @@ import fs from "fs";
     if (!image_url) {
       return res.status(422).send({ message: 'Unprocessable Entity' });
     }
-
-    // get the filtered image result
-    const filteredImage: string = await filterImageFromURL(image_url)
+    let filteredImage: string
+    try {
+      // get the filtered image result
+      filteredImage = await filterImageFromURL(image_url)
+    } catch (error) {
+      console.log("Image parsing error :", error)
+      return res.status(500).send({ messag: `this url: ${image_url} can not be processed` })
+    }
 
     res.sendFile(filteredImage, (err) => {
       if (err) {
@@ -59,8 +49,6 @@ import fs from "fs";
       }
     });
   });
-
-  //! END @TODO1
 
   // Root Endpoint
   // Displays a simple message to the user
