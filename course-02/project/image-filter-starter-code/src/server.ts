a78de75fcb3f3d4282a0352eb87bd1e1443cd7f2
Ayabonga <ayabongabooi2@gmail.com>
2022-05-22 17:32:34 +0200

    refactor(server.ts): [] updated code to use typecript variables and handle error better

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -16,27 +16,31 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
   //   GET /filteredimage?image_url={{URL}}
   //  endpoint to filter an image from a public url.
-  app.get('/filteredimage', async (req, res) => {
-    let {image_url} = req.query;
-    if (!image_url) {
-      return res.status(400).send({ message: 'image_url is required' });
-    }
-    // make sure the image_url is a valid URL
-    if(!image_url.startsWith('http') || !image_url.startsWith('https')) {
-      return res.status(400).send({ message: 'image_url must start with http or https' });
-    }
+  app.get('/filteredimage', async (req: express.Request, res: express.Response) => {
+      try {
+        const  {image_url} :{image_url: string} = req.query;
+        if (!image_url) {
+          return res.status(400).send({ message: 'image_url is required' });
+        }
+        // make sure the image_url is a valid URL
+        if(!image_url.startsWith('http') || !image_url.startsWith('https')) {
+          return res.status(400).send({ message: 'image_url must start with http or https' });
+        }
 
-    const filteredpath = await filterImageFromURL(image_url);
-    res.status(200).sendFile(filteredpath, () => {
+        const filteredpath = await filterImageFromURL(image_url);
+        res.status(200).sendFile(filteredpath, () => {
 
-      deleteLocalFiles([filteredpath]);
-    });
+          deleteLocalFiles([filteredpath]);
+        });
+      } catch (error) {
+        res.status(500).send({ message:"Something went wrong!, unable to save the image."})
+      }
   }
   );
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get("/", async (req, res) => {
+  app.get("/", async (req: express.Request, res: express.Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
   });
 
