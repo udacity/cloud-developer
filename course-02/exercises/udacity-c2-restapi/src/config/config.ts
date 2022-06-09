24dc0e4483564605a55e0ccc44a8bfae2ece56e5
Christian Onwe <chrisoftech@gmail.com>
2022-06-09 17:23:24 +0100

    Setup initial configuration for running project.

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramDBDev",
+    "password": "11111111",
+    "database": "udagramDBDev",
+    "host": "udagramdbdev.cj8zmco2l4nc.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-1234567890-bucket-dev"
   },
   "jwt": {
     "secret": " "
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/user.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/user.router.ts
@@ -8,6 +8,7 @@ const router: Router = Router();
 router.use('/auth', AuthRouter);
 
 router.get('/', async (req: Request, res: Response) => {
+    res.send([]);
 });
 
 router.get('/:id', async (req: Request, res: Response) => {
