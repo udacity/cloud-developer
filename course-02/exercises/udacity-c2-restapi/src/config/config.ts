8c6b6e62d997dc32cd618cd75de9ce74f5b0498f
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2022-06-11 09:36:36 +0100

    updated working packages and setup aws ebcli

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,22 +1,24 @@
+require("dotenv").config();
+
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_NAME,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    aws_region: process.env.AWS_REGION,
+    aws_profile: process.env.AWS_PROFILE,
+    aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
   },
-  "jwt": {
-    "secret": " "
+  jwt: {
+    secret: " ",
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres",
+  },
+};
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -1,70 +1,82 @@
-import { Router, Request, Response } from 'express';
-import { FeedItem } from '../models/FeedItem';
-import { requireAuth } from '../../users/routes/auth.router';
-import * as AWS from '../../../../aws';
+import { Router, Request, Response } from "express";
+import { FeedItem } from "../models/FeedItem";
+import { requireAuth } from "../../users/routes/auth.router";
+import * as AWS from "../../../../aws";
 
 const router: Router = Router();
 
 // Get all feed items
-router.get('/', async (req: Request, res: Response) => {
-    const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
-    items.rows.map((item) => {
-            if(item.url) {
-                item.url = AWS.getGetSignedUrl(item.url);
-            }
-    });
-    res.send(items);
+router.get("/", async (req: Request, res: Response) => {
+  const items = await FeedItem.findAndCountAll({ order: [["id", "DESC"]] });
+  items.rows.map(item => {
+    if (item.url) {
+      item.url = AWS.getGetSignedUrl(item.url);
+    }
+  });
+  res.send(items);
 });
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get("/:id", async (req: Request, res: Response) => {
+  let { id } = req.params;
 
-// update a specific resource
-router.patch('/:id', 
-    requireAuth, 
-    async (req: Request, res: Response) => {
-        //@TODO try it yourself
-        res.status(500).send("not implemented")
+  if (!id) {
+    res.status(400).send("please input a valid primary key");
+  }
+  const item = await FeedItem.findByPk(id);
+
+  res.status(200).send(item);
 });
-
+// update a specific resource
+router.patch("/:id", requireAuth, async (req: Request, res: Response) => {
+  //@TODO try it yourself
+  res.status(500).send("not implemented");
+});
 
 // Get a signed url to put a new item in the bucket
-router.get('/signed-url/:fileName', 
-    requireAuth, 
-    async (req: Request, res: Response) => {
+router.get(
+  "/signed-url/:fileName",
+  requireAuth,
+  async (req: Request, res: Response) => {
     let { fileName } = req.params;
     const url = AWS.getPutSignedUrl(fileName);
-    res.status(201).send({url: url});
-});
+    res.status(201).send({ url: url });
+  }
+);
 
-// Post meta data and the filename after a file is uploaded 
+// Post meta data and the filename after a file is uploaded
 // NOTE the file name is they key name in the s3 bucket.
 // body : {caption: string, fileName: string};
-router.post('/', 
-    requireAuth, 
-    async (req: Request, res: Response) => {
-    const caption = req.body.caption;
-    const fileName = req.body.url;
+router.post("/", requireAuth, async (req: Request, res: Response) => {
+  const caption = req.body.caption;
+  const fileName = req.body.url;
 
-    // check Caption is valid
-    if (!caption) {
-        return res.status(400).send({ message: 'Caption is required or malformed' });
-    }
+  // check Caption is valid
+  if (!caption) {
+    return res
+      .status(400)
+      .send({ message: "Caption is required or malformed" });
+  }
 
-    // check Filename is valid
-    if (!fileName) {
-        return res.status(400).send({ message: 'File url is required' });
-    }
+  // check Filename is valid
+  if (!fileName) {
+    return res.status(400).send({ message: "File url is required" });
+  }
 
-    const item = await new FeedItem({
-            caption: caption,
-            url: fileName
-    });
+  // const item = await new FeedItem({
+  //         caption: caption,
+  //         url: fileName
+  // });
+  const item: FeedItem = FeedItem.build({
+    caption: caption,
+    url: fileName,
+  });
 
-    const saved_item = await item.save();
+  const saved_item = await item.save();
 
-    saved_item.url = AWS.getGetSignedUrl(saved_item.url);
-    res.status(201).send(saved_item);
+  saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+  res.status(201).send(saved_item);
 });
 
 export const FeedRouter: Router = router;
