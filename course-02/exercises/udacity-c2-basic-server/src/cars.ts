b8a65d7c41cfbe617dff8dff8d10c1f50c786ca6
efuraimujs <efuraimu.js@gmail.com>
2022-06-12 17:22:58 +0300

    server-setup-1655043572

--- course-02/exercises/udacity-c2-basic-server/src/cars.ts
+++ course-02/exercises/udacity-c2-basic-server/src/cars.ts
@@ -7,8 +7,8 @@ export interface Car {
 }
 
 export const cars: Car[] = [
-    { make: 'tesla', type: 'sedan', model: 'roadster', cost: 33, id: 0 },
-    { make: 'tesla', type: 'suv', model: 'model 3', cost: 48, id: 1 },
-    { make: 'toyota', type: 'sedan', model: 'prius', cost: 22, id: 2 },
-    { make: 'honda', type: 'sedan', model: 'civic', cost: 22, id: 3 }
+    { make: 'tesla', type: 'sedan', model: 'roadster', cost: 33, id: 1 },
+    { make: 'tesla', type: 'suv', model: 'model 3', cost: 48, id: 2 },
+    { make: 'toyota', type: 'sedan', model: 'prius', cost: 22, id: 3 },
+    { make: 'honda', type: 'sedan', model: 'civic', cost: 22, id: 4 }
    ]
