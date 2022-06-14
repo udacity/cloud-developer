ec06ce48fe8c22093466e602191042103026d584
efuraimujs <efuraimu.js@gmail.com>
2022-06-14 03:49:14 +0300

    endpoint to GET a specific resource by PK

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,6 +18,12 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async(req: Request, res: Response) => {
+    let {id} = req.params
+    const item = await FeedItem.findByPk(id)
+    res.send(item)
+})
+
 
 // update a specific resource
 router.patch('/:id', 
