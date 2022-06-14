d1ee1f3f75d9cd5ee35e8064e4f9820a301b2a4f
efuraimujs <efuraimu.js@gmail.com>
2022-06-15 02:32:03 +0300

    eb-aws deployment

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,3 +1,26 @@
+// export const config = {
+//   "dev": {
+//     "username": "process.env.POSTGRESS_USERNAME",
+//     "password": "process.env.POSTGRESS_PASSWORD",
+//     "database": "process.env.POSTGRESS_DATABASE",
+//     "host": "process.env.POSTGRESS_HOST",
+//     "dialect": "postgres",
+//     "aws_region": "process.env.AWS_REGION",
+//     "aws_profile": "process.env.AWS_PROFILE",
+//     "aws_media_bucket": "process.env.AWS_MEDIA_BUCKET"
+//   },
+//   "jwt": {
+//     "secret": " "
+//   },
+//   "prod": {
+//     "username": "",
+//     "password": "",
+//     "database": "udagram_prod",
+//     "host": "",
+//     "dialect": "postgres"
+//   }
+// }
+
 export const config = {
   "dev": {
     "username": "rootefuraimujs",
@@ -5,18 +28,18 @@ export const config = {
     "database": "udagramdb1655121080",
     "host": "udagramdb1655121080.cocqdrux33zf.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagrambucket1655121080"
   },
   "jwt": {
     "secret": " "
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
+    "username": "rootefuraimujs",
+    "password": "udagramdb1655121080",
+    "database": "udagramdb1655121080",
+    "host": "udagramdb1655121080.cocqdrux33zf.us-east-1.rds.amazonaws.com",
     "dialect": "postgres"
   }
 }
--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -1,3 +1,5 @@
+require('dotenv').config()
+// console.log(process.env)
 import express from 'express';
 import { sequelize } from './sequelize';
 
@@ -6,6 +8,7 @@ import { IndexRouter } from './controllers/v0/index.router';
 import bodyParser from 'body-parser';
 
 import { V0MODELS } from './controllers/v0/model.index';
+
 
 (async () => {
   await sequelize.addModels(V0MODELS);
