c19f4955c9a6aa18332f71c60751c992b6781580
DENNICE NDEDE <dennicendede@gmail.com>
2022-06-20 02:04:15 -0700

    Update server.ts

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -30,7 +30,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get("/filteredimage", async (req, res) => {
+    const {image_url} = req.query;
+    
+    if(!image_url) {
+      return res.status(400).json({"Provide a valid image url": false});
+    }
+    try {
+      let imageFile = await filterImageFromURL(image_url);
+      console.log(imageFile);
+
+      return res.status(200).sendFile(imageFile, () => {
+        deleteLocalFiles([imageFile]);
+      });
+    } catch (err) {
+      return res.status(422).send("Unable to download this file.");
+    }
+  });
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
