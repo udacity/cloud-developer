1851e7515d983b115d74d961c54235a4fbf4710d
Dimkpa Chimenim <Dimkpachime@gmail.com>
2022-06-22 23:31:08 +0100

    Dimkpa's-image-filter-app

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -7,29 +7,29 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Init the Express application
   const app = express();
 
-  // Set the network port
+  // Set the network port 
   const port = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage", async (req, res) => {
+    const {image_url} = req.query;
+    
+    if(!image_url) {
+      return res.status(400).json({"Provide a valid image url": false});
+    }
+    try {
+      let imageFile = await filterImageFromURL(image_url);
+      console.log(imageFile);
 
-  /**************************************************************************** */
-
-  //! END @TODO1
+      return res.status(200).sendFile(imageFile, () => {
+        deleteLocalFiles([imageFile]);
+      });
+    } catch (err) {
+      return res.status(422).send("Unable to download this file.");
+    }
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
