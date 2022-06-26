97a335bafecc73c63704559218ffe6b5aa8b0916
jason-ibisi <jasonibisi@gmail.com>
2022-06-26 07:21:27 +0100

    feat: added cars endpoint

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -4,35 +4,33 @@ import { expect } from 'chai';
 import 'mocha';
 
 describe('add function', () => {
-
   it('should add two and two', () => {
-    const result = add(2,2);
+    const result = add(2, 2);
     expect(result).to.equal(4);
   });
 
   it('should add -2 and two', () => {
-    const result = add(-2,2);
+    const result = add(-2, 2);
     expect(result).to.equal(0);
   });
-
 });
 
 describe('divide', () => {
-
   it('should divide 6 by 3', () => {
-    const result = divide(6,3);
+    const result = divide(6, 3);
     expect(result).to.equal(2);
   });
 
   it('should divide 5 and 2', () => {
-    const result = divide(5,2);
+    const result = divide(5, 2);
     expect(result).to.equal(2.5);
   });
 
   it('should throw an error if div by zero', () => {
-    expect(()=>{ divide(5,0) }).to.throw('div by 0')
+    expect(() => {
+      divide(5, 0);
+    }).to.throw('div by 0');
   });
-
 });
 
 // @TODO try creating a new describe block for the "concat" method
--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -1,17 +1,26 @@
 // Super Simple Unit Functions
 
 export const add = (a: number, b: number) => {
-    return a + b;
-    }
+  return a + b;
+};
 
 export const divide = (a: number, b: number) => {
-    if(b === 0) { throw new Error('div by 0') }
+  if (b === 0) {
+    throw new Error('div by 0');
+  }
 
-    return a / b;
-    }
+  return a / b;
+};
 
 // @TODO try creating a method "concat" to concatenate two strings
 // it should take two string paramaters.
 // it should return one string combining the two strings.
 // it should throw an error if either of the strings are empty.
 // ensure your function is exported.
+export const concat = (a: string, b: string) => {
+  if (a === '' || a === null || b == '' || b === null) {
+    throw new Error('empty string');
+  }
+
+  return ''.concat(a, ' ', b);
+};
