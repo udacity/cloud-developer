075ef0cd531dc9df6b875a85d891302220db8d19
Huber, Benjamin <benjamin.huber@empa.ch>
2022-07-03 19:17:06 +0200

    f-auth implemented

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -10,7 +10,7 @@ export const config = {
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
-    "secret": " "
+    "secret": process.env.JWT_SECRET
   },
   "prod": {
     "username": "",
