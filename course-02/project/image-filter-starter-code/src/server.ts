f7585cdbf3120facc8aa28414ad285bbfce61c43
Huber, Benjamin <benjamin.huber@empa.ch>
2022-07-06 13:31:58 +0200

    imageFilter endpoint added

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -17,17 +17,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
+  //    1. validate the image_url query -> Check
+  //    2. call filterImageFromURL(image_url) to filter the image -> Check
+  //    3. send the resulting file in the response -> Check
+  //    4. deletes any files on the server on finish of the response -> Check
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
+  app.get("/filteredImageFromURL/", async (req, res ) => {
+    const { image_url } = req.query;
+
+    // Validate URL
+    if ( image_url.substring(0,7) != 'http://' && image_url.substring(0,8) != 'https://' ) {
+      return res.status(400)
+                .send(`URL malformed`);
+    }
+    const filteredpath = await filterImageFromURL(image_url);
+    return res.status(200).sendFile(filteredpath, async() => deleteLocalFiles([filteredpath]));
+  })
 
   //! END @TODO1
   
