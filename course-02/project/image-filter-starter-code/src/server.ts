4fe962f9732666f3ad985bf17e5ece191852fa18
DENNICE NDEDE <dennicendede@gmail.com>
2022-07-09 21:01:26 -0700

    output

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,3 +1,4 @@
+import {Request, Response} from 'express';
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
@@ -28,7 +29,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage/",async (req: Request,res: Response)=>{
+    let {image_url}: any = req.query;
+    if( !image_url ) {
+      return res.status(422)
+                .send(`Requested Image can not be processed`);
+    }else{
+        filterImageFromURL(image_url).then((result)=>{
 
+        res.sendFile(result);
+        res.on(`finish`,()=>deleteLocalFiles([result]));
+        }).catch((err)=>res.status(422).send(err))
+      }
+  } );
   //! END @TODO1
   app.get("/filteredimage", async (req, res) => {
     const {image_url} = req.query;
