491d79b7d8aba3674f7330291151e751db9b46c3
Olatunji Yusuf <olatunjiyso@gmail.com>
2022-07-26 19:14:40 +0100

    fix: set type for caught exception e

--- course-04/project/c4-final-project-starter-code/client/src/components/EditTodo.tsx
+++ course-04/project/c4-final-project-starter-code/client/src/components/EditTodo.tsx
@@ -58,7 +58,7 @@ export class EditTodo extends React.PureComponent<
 
       alert('File was uploaded!')
     } catch (e) {
-      alert('Could not upload a file: ' + e.message)
+      alert('Could not upload a file: ' + (e as Error).message)
     } finally {
       this.setUploadState(UploadState.NoUpload)
     }
