d75046c271ade912a4de1c349aef5973e8f1ec41
jlerocher <jlerocher@gmail.com>
2022-08-12 16:15:41 +0100

    Mes fichiers validés

--- course-02/exercises/udacity-c2-basic-server/src/cars.ts
+++ course-02/exercises/udacity-c2-basic-server/src/cars.ts
@@ -3,12 +3,12 @@ export interface Car {
     type: string;
     model: string;
     cost: number;
-    id: number;
+    id: string;
 }
 
 export const cars: Car[] = [
-    { make: 'tesla', type: 'sedan', model: 'roadster', cost: 33, id: 0 },
-    { make: 'tesla', type: 'suv', model: 'model 3', cost: 48, id: 1 },
-    { make: 'toyota', type: 'sedan', model: 'prius', cost: 22, id: 2 },
-    { make: 'honda', type: 'sedan', model: 'civic', cost: 22, id: 3 }
+    { make: 'tesla', type: 'sedan', model: 'roadster', cost: 33, id: "0" },
+    { make: 'tesla', type: 'suv', model: 'model 3', cost: 48, id: "1" },
+    { make: 'toyota', type: 'sedan', model: 'prius', cost: 22, id: "2" },
+    { make: 'honda', type: 'sedan', model: 'civic', cost: 22, id: "3" }
    ]
