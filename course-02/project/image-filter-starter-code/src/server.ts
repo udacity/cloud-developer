6faf46289d744367313bb2c4ee122e795d3b95ae
jlerocher <jlerocher@gmail.com>
2022-08-15 01:42:38 +0100

    Create a new endpoint in the server.ts file

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,35 +1,33 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+// Imports
+import express, { Request, Response } from "express";
+import { isUri } from "valid-url";
+import bodyParser from "body-parser";
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
 
-  // Init the Express application
+// Init the Express application
   const app = express();
 
-  // Set the network port
+// Set the network port
   const port = process.env.PORT || 8082;
   
-  // Use the body parser middleware for post requests
+  const my_filter = filterImageFromURL
+  const del = deleteLocalFiles
+// Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+//@ToDo 1: This is the endpoint for image filterer
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    const { image_url: imageUrl } = req.query;
+    if (!imageUrl || !isUri(imageUrl)) {
+      return res.status(400).send({ auth: false, message: 'Image url is missing or malformed' });
+    }
 
-  /**************************************************************************** */
+    const filteredPath = await my_filter(imageUrl);
 
-  //! END @TODO1
+    res.sendFile(filteredPath, {}, () => del([filteredPath]));
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
