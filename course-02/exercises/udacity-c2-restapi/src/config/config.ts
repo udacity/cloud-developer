03899d8872230ee690c28389c9a0b9fa7f3e9ce4
oladejo-michael <mickeyboy16@gmail.com>
2022-08-17 15:15:46 +0100

    Completion of backend development

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,15 @@
+import { env } from "process";
+
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": process.env.POSTGRES_REGION,
+    "aws_profile": process.env.POSTGRES_PROFILE,
+    "aws_media_bucket": "udagrammikedev"
   },
   "jwt": {
     "secret": " "
