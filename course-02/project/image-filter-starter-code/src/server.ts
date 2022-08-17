7e8ed3727463ca11f411eeda501a343fce0e738d
TatchumNono <Tatchumnono@gmail.com>
2022-08-17 22:55:12 +0100

    TODO 1 done

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,46 +1,70 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
 (async () => {
+	// Init the Express application
+	const app = express();
 
-  // Init the Express application
-  const app = express();
+	// Set the network port
+	const port = process.env.PORT || 8082;
 
-  // Set the network port
-  const port = process.env.PORT || 8082;
-  
-  // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+	// Use the body parser middleware for post requests
+	app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+	// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+	// GET /filteredimage?image_url={{URL}}
+	// endpoint to filter an image from a public url.
+	// IT SHOULD
+	//    1
+	//    1. validate the image_url query
+	//    2. call filterImageFromURL(image_url) to filter the image
+	//    3. send the resulting file in the response
+	//    4. deletes any files on the server on finish of the response
+	// QUERY PARAMATERS
+	//    image_url: URL of a publicly accessible image
+	// RETURNS
+	//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
+	/**************************************************************************** */
 
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+	//! END @TODO1
 
-  // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+	app.get("/filteredimage", async (req: Request, res: Response) => {
+		const { image_url } = req.query;
+
+		let filteredpath: string;
+
+		let files: Array<string> = [];
+
+		if (!image_url) {
+			return res.status(401).send("image_url is required");
+		}
+
+		filterImageFromURL(image_url)
+			.then((result) => {
+				filteredpath = result;
+
+				res.status(200).sendFile(filteredpath);
+
+				files.push(filteredpath);
+			})
+			.catch((err) => {
+				res.status(500).send(err.toString());
+			});
+
+		await deleteLocalFiles(files);
+	});
+
+	// Root Endpoint
+	// Displays a simple message to the user
+	app.get("/", async (req, res) => {
+		res.send("try GET /filteredimage?image_url={{}}");
+	});
+
+	// Start the Server
+	app.listen(port, () => {
+		console.log(`server running http://localhost:${port}`);
+		console.log(`press CTRL+C to stop server`);
+	});
 })();
