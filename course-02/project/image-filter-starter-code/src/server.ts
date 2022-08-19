ba80101a3ccf2c767d94a76dc0038130a7d6043f
oladejo-michael <mickeyboy16@gmail.com>
2022-08-19 20:22:19 +0100

    Image filter endpoints added

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {Router, Request, Respond} from 'express'
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -17,18 +18,28 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
+
+  /**************************************************************************** */
+  app.get("/filteredimage", async(req: Request, res: Response) => {
+    const image_url = req.query.image_url.toString();
+    if (!image_url) {
+      res.status(400).send("Image url is required");
+    }
+  
+    const filtered_image = await filterImageFromURL(image_url);
+
   //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
+    res.status(200).sendFile(filtered_image, () => {
+      deleteLocalFiles([filtered_image]);
+    })
+  })
   //! END @TODO1
   
   // Root Endpoint
