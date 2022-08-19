e725df86230da69016a7f93d41f8fcb4c441eb5c
Idris21 <idris.taw@gmail.com>
2022-08-19 17:32:22 +0100

    Remove @todo comment and added comment for /filteredimage endpoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,25 +13,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  // working url = https://www.humanesociety.org/sites/default/files/styles/1441x612/public/2022-07/kitten-playing-575035.jpg
-  // https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg
+  // Implement /filterimage endpoint
+  // upload and process an image from url 
   app.get("/filteredimage", async (req, res) => {
     
     const { image_url } = req.query;
@@ -41,12 +24,12 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     }
 
     const filtered_image_path = await filterImageFromURL(image_url)
-    .then((img) => {
-      res.status(200).sendFile(img, (err) => {
+    .then((image_path) => {
+      res.status(200).sendFile(image_path, (err) => {
         if (err) {
           return res.status(500);
         }
-        deleteLocalFiles([img])
+        deleteLocalFiles([image_path])
       });
 
     })
