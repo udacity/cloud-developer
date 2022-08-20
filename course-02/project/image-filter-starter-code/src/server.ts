d5d5011b1842d09629a6b789e516ac44dc7550ff
Wakili35 <wakili35@gmail.com>
2022-08-20 11:48:39 +0100

    commit on my_stage

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -15,12 +15,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
+  app.get("/filteredimage", async (req, res) => {
+    const { image_url } = req.query;
+
+    if (!image_url) {
+      return res.status(400).send("Image is not verified");
+    }
+    const ImageFiltered = await filterImageFromURL(image_url);
+    return res.status(200).sendFile(ImageFiltered);
+    
+  });
   //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
