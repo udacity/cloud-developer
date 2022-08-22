b86283a2f01a437fa89e774803134adb43c301cd
Wakili35 <wakili35@gmail.com>
2022-08-22 09:09:13 +0100

    commint on 'my-stage'

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,29 +13,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
+  
+  ///////// @TODO1 IMPLEMENT A RESTFUL ENDPOINT ///////////////
   app.get("/filteredimage", async (req, res) => {
     const { image_url } = req.query;
 
     if (!image_url) {
-      return res.status(400).send("Image is not verified");
-    }
-    const ImageFiltered = await filterImageFromURL(image_url);
-    return res.status(200).sendFile(ImageFiltered);
-    
+      return res.status(422).send("Image is not verified");
+    } 
+    const ImagePath = await filterImageFromURL(image_url);
+    res.status(200).sendFile(ImagePath);
+    return deleteLocalFiles([ImagePath]);
   });
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
-  // Root Endpoint
+  ////////////// Root Endpoint //////////////////////////////
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
