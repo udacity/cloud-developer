e0c0d7e224b169613b218faccf617e2d26b34168
0xceejay <0xceejay@gmail.com>
2022-08-23 11:51:15 +0100

    Update config.ts

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "ceejaydb",
+    "password": "0xceejaydb",
+    "database": "ceejaydb",
+    "host": "ceejaydb.cmydjrbiczif.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": ""
   },
   "jwt": {
     "secret": " "
