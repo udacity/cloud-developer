8a987dd9bb2c4a9e3e80da1761efe33ea08fff29
Oyinbayode <oyinbayode75@gmail.com>
2022-08-25 22:23:38 +0100

    Finalisimo

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,15 @@
+import "dotenv/config";
+
 export const config = {
   dev: {
-    username: "postgres",
-    password: "ba1ya4da2",
-    database: "udacitydb",
-    host: "udacitydb.cihhqfue35of.us-east-1.rds.amazonaws.com",
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     dialect: "postgres",
-    aws_region: "us-east-1",
-    aws_profile: "default",
-    aws_media_bucket: "udagram-oyin-dev",
+    aws_region: process.env.AWS_REGION,
+    aws_profile: process.env.AWS_PROFILE,
+    aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
   },
   jwt: {
     secret: " ",
