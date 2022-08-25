9f4e5046c59f3e97e0c95538621dd3251374a9f9
Hassan <hassantarek201517@gmail.com>
2022-08-25 21:14:13 +0200

    auth

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -10,7 +10,7 @@ export const config = {
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
-    "secret": " "
+    "secret": "hello world"
   },
   "prod": {
     "username": "",
