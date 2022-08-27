479cdcebe3f9e39c197aff33d617e8635ec92fce
0xceejay <0xceejay@gmail.com>
2022-08-27 15:02:29 +0100

    corrections

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,7 +13,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get("/filteredimage", async(req, res) => {
+  app.get("/filteredimage", async(req: express.Request, res: express.Response) => {
        let { image_url } :{image_url:string} = req.query;
 
     if ( !image_url ) {
