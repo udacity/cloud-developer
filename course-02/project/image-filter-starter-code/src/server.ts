a0ff7c26515d68a61aa45d6d717fce620c60dcf7
vianneyKB <36236520+vianneyKB@users.noreply.github.com>
2022-08-28 12:00:21 +0200

    filter an image from a public url

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Router, Request, Response} from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -28,6 +29,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+
+  app.get('/filteredimage', async (req: Request, res: Response) => {
+    const image_url = req.query.image_url.toString();
+
+    if (!image_url) {
+      res.sendStatus(400).send({ message: 'image url is required' });
+    }
+
+    const filterimage = await filterImageFromURL(image_url);
+
+    if (!filterimage) {
+      return res.status(404).send({ message: 'bad request' });
+    }
+    res.status(200).sendFile(filterimage, () => {
+      deleteLocalFiles([filterimage])
+    })
+  }) 
 
   //! END @TODO1
   
