2276d3c297f1d1c2ac63f77b6087f2d0083b0d3a
Joel Obowu <jobowu@usiu.ac.ke>
2022-08-30 00:18:57 +0300

    Working Code

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, {Router, Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { request } from 'http';
 
 (async () => {
 
@@ -28,6 +29,32 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+
+
+app.get("/filteredimage", async(req: Request, res: Response) => {
+
+  let image_url: string = req.query.image_url;
+  const isValidUrl: (string)[] = image_url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  if(isValidUrl == null){
+    return res.status(400).send(`Invalid url! Try again with valid url`);
+  }
+  else{
+  //Process Image
+    const filteredImage: string = await filterImageFromURL(image_url);
+    if(filteredImage===undefined||filteredImage===null){
+
+      return res.status(400).send(`Unable to filter image`);
+
+    }
+  else
+    res.status(200).sendFile(filteredImage, function(){
+      deleteLocalFiles([filteredImage])
+    });
+    }
+
+});
+
+
 
   //! END @TODO1
   
